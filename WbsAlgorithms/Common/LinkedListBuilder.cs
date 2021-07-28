b43bd1f48731eea22995abc28491341e420fda5a
Leszek Taratuta <ata6502@hotmail.com>
2021-07-27 21:52:26 -0400

    Add CreateDoublyLinkedList to linked list builder

--- WbsAlgorithms/Common/LinkedListBuilder.cs
+++ WbsAlgorithms/Common/LinkedListBuilder.cs
@@ -3,7 +3,7 @@
     public class LinkedListBuilder
     {
         /// <summary>
-        /// Create a singly-linked linked list and populate it with given items.
+        /// Creates a singly-linked list and populates it with given items.
         /// </summary>
         /// <param name="items">Items to populate the linked list</param>
         /// <returns>The head of the newly created linked list</returns>
@@ -20,6 +20,34 @@
                     head = node;
                 else
                     prev.Next = node;
+
+                prev = node;
+            }
+
+            return head;
+        }
+
+        /// <summary>
+        /// Creates a doubly-linked list and populates it with given items.
+        /// </summary>
+        /// <param name="items">Items to populate the linked list</param>
+        /// <returns>The head of the newly created linked list</returns>
+        public static DoubleListNode<T> CreateDoublyLinkedList<T>(T[] items)
+        {
+            DoubleListNode<T> head = null;
+            DoubleListNode<T> prev = null;
+
+            foreach (var item in items)
+            {
+                var node = new DoubleListNode<T>(item);
+
+                if (prev == null)
+                    head = node;
+                else
+                {
+                    prev.Next = node;
+                    node.Prev = prev;
+                }
 
                 prev = node;
             }
