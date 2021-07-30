a0302e03ee0275707cda6a4b96e7d7082847f6b0
Leszek Taratuta <ata6502@hotmail.com>
2021-07-29 20:49:53 -0400

    Add steque data structure

---
+++ WbsAlgorithms/Collections/StequeLinkedList.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WbsAlgorithms.Common;
+
+namespace WbsAlgorithms.Collections
+{
+    /// <summary>
+    /// Steque is a stack-ended queue. It supports Push, Pop, and Enqueue operations.
+    /// This implementation uses a singly-linked list to store items.
+    /// Knuth calls the steque an output-restricted deque.
+    /// 
+    /// [Sedgewick] 1.3.32 p.167 - Implement a steque data structure with the following operations:
+    /// - Add an item to the top of the steque (Push).
+    /// - Remove the most recently added item (Pop).
+    /// - Add an item to the bottom of the steque (Enqueue).
+    /// </summary>
+    public class StequeLinkedList<T>
+    {
+        private ListNode<T> _head; // a pointer to the least recently added node; the top of the steque
+        private ListNode<T> _tail; // a pointer to the most recently added node; the bottom of the steque
+
+        /// <summary>
+        /// Indicates whether the steque is empty.
+        /// </summary>
+        public bool IsEmpty => (Size == 0 && _head == null && _tail == null);
+
+        /// <summary>
+        /// Returns the number of items in the steque.
+        /// </summary>
+        public int Size { get; private set; }
+
+        /// <summary>
+        ///  Adds an item to the top of the steque.
+        /// </summary>
+        /// <param name="item">An item to add</param>
+        public void Push(T item)
+        {
+            // Create a new head of the linked list providing the current head as its next node.
+            var newHead = new ListNode<T>(item, _head);
+
+            // Check if this is the first node. If so, adjust the last node.
+            if (_head == null)
+                _tail = newHead;
+
+            // Assign the list's head to the new node i.e., add the item to the top of the steque.
+            _head = newHead;
+
+            ++Size;
+        }
+
+        /// <summary>
+        /// Removes the most recently added item.
+        /// </summary>
+        /// <returns>The most recently added item</returns>
+        public T Pop()
+        {
+            // Throw an exception if the steque is empty.
+            if (IsEmpty)
+                throw new NullReferenceException("The steque is empty.");
+
+            // Keep the value of the linked list's head. We need to return it.
+            var item = _head.Item;
+
+            // Remove the node from the beginning of the linked list.
+            _head = _head.Next;
+
+            // Check if there are any nodes in the linked list. If not, adjust the last node.
+            if (_head == null)
+                _tail = null;
+
+            --Size;
+
+            return item;
+        }
+
+        /// <summary>
+        /// Adds an item to the bottom of the steque.
+        /// </summary>
+        /// <param name="item">The item to add</param>
+        public void Enqueue(T item)
+        {
+            // Create a new node.
+            var newNode = new ListNode<T>(item, null);
+
+            if (IsEmpty)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                // Add the new node at the end of the linked list.
+                _tail.Next = newNode;
+                _tail = newNode;
+            }
+
+            ++Size;
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Collections/StequeTest.cs
@@ -0,0 +1,195 @@
+﻿using NUnit.Framework;
+using System;
+using WbsAlgorithms.Collections;
+
+namespace WbsAlgorithmsTest.Collections
+{
+    [TestFixture]
+    public class StequeTest
+    {
+        [Test]
+        public void EmptyStequeTest()
+        {
+            var s = new StequeLinkedList<int>();
+
+            Assert.IsTrue(s.IsEmpty);
+            Assert.AreEqual(0, s.Size);
+
+            // If the steque is empty, it should throw an exception.
+            Assert.Throws<NullReferenceException>(() => s.Pop());
+        }
+
+        [Test]
+        public void PushPopOneElementTest()
+        {
+            var s = new StequeLinkedList<int>();
+
+            s.Push(1);
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(1, s.Size);
+
+            var val = s.Pop();
+            Assert.AreEqual(1, val);
+        }
+
+        [Test]
+        public void PushPopTwoElementsTest()
+        {
+            var s = new StequeLinkedList<int>();
+
+            s.Push(1);
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(1, s.Size);
+
+            s.Push(2);
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(2, s.Size);
+
+            var val = s.Pop();
+            Assert.AreEqual(2, val);
+
+            val = s.Pop();
+            Assert.AreEqual(1, val);
+        }
+
+        [Test]
+        public void PushPopThreeElementsTest()
+        {
+            var s = new StequeLinkedList<int>();
+
+            s.Push(1);
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(1, s.Size);
+
+            s.Push(2);
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(2, s.Size);
+
+            s.Push(3);
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(3, s.Size);
+
+            var val = s.Pop();
+            Assert.AreEqual(3, val);
+
+            val = s.Pop();
+            Assert.AreEqual(2, val);
+
+            val = s.Pop();
+            Assert.AreEqual(1, val);
+        }
+
+        [Test]
+        public void PushPopEnqueueMultipleElementsTest()
+        {
+            var s = new StequeLinkedList<int>();
+
+            s.Push(1);
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(1, s.Size);
+
+            s.Push(2);
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(2, s.Size);
+
+            s.Enqueue(3);
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(3, s.Size);
+
+            var v = s.Pop();
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(2, s.Size);
+            Assert.AreEqual(2, v);
+
+            v = s.Pop();
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(1, s.Size);
+            Assert.AreEqual(1, v);
+
+            v = s.Pop();
+
+            Assert.IsTrue(s.IsEmpty);
+            Assert.AreEqual(0, s.Size);
+            Assert.AreEqual(3, v);
+
+            Assert.Throws<NullReferenceException>(() => s.Pop());
+
+            s.Enqueue(4);
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(1, s.Size);
+
+            s.Enqueue(5);
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(2, s.Size);
+
+            v = s.Pop();
+
+            Assert.IsFalse(s.IsEmpty);
+            Assert.AreEqual(1, s.Size);
+            Assert.AreEqual(4, v);
+
+            v = s.Pop();
+
+            Assert.IsTrue(s.IsEmpty);
+            Assert.AreEqual(0, s.Size);
+            Assert.AreEqual(5, v);
+        }
+
+        [Test]
+        public void EnqueuePopTest()
+        {
+            var s = new StequeLinkedList<int>();
+
+            s.Enqueue(10);
+            s.Enqueue(11);
+            s.Enqueue(12);
+
+            Assert.AreEqual(10, s.Pop());
+            Assert.AreEqual(11, s.Pop());
+            Assert.AreEqual(12, s.Pop());
+        }
+
+        [Test]
+        public void MixedPushPopEnqueTest()
+        {
+            var s = new StequeLinkedList<int>();
+
+            s.Enqueue(1);   // 1
+            s.Push(2);      // 2,1
+            s.Enqueue(3);   // 2,1,3
+            s.Enqueue(4);   // 2,1,3,4
+            s.Enqueue(5);   // 2,1,3,4,5
+            s.Push(6);      // 6,2,1,3,4,5
+            s.Push(7);      // 7,6,2,1,3,4,5
+            s.Enqueue(8);   // 7,6,2,1,3,4,5,8
+            s.Push(9);      // 9,7,6,2,1,3,4,5,8
+            s.Enqueue(10);  // 9,7,6,2,1,3,4,5,8,10
+
+            Assert.AreEqual(9, s.Pop());
+            Assert.AreEqual(7, s.Pop());
+            Assert.AreEqual(6, s.Pop());
+            Assert.AreEqual(2, s.Pop());
+            Assert.AreEqual(1, s.Pop());
+            Assert.AreEqual(3, s.Pop());
+            Assert.AreEqual(4, s.Pop());
+            Assert.AreEqual(5, s.Pop());
+            Assert.AreEqual(8, s.Pop());
+            Assert.AreEqual(10, s.Pop());
+
+            Assert.Throws<NullReferenceException>(() => s.Pop());
+        }
+    }
+}
