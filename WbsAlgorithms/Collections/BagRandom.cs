dd14a1ddc2f210f04bf4a26b40573a0871704cd5
Leszek Taratuta <ata6502@hotmail.com>
2021-08-03 16:54:43 -0400

    Add a random bag data structure

---
+++ WbsAlgorithms/Collections/BagRandom.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+
+namespace WbsAlgorithms.Collections
+{
+    /// <summary>
+    /// A random bag stores a collection of items and supports the Add(item) operation.
+    /// Random bag iterations provide the items in random order. All N! permutations are 
+    /// equally likely.
+    /// 
+    /// [Sedgewick] 1.3.34 p.167 - Implement a random bag collection.
+    /// </summary>
+    public class BagRandom<T>
+    {
+        private Random _rnd = new Random();
+
+        // The random bag keeps the items in an array and randomizes the order of the items
+        // in the GetEnumerator method.
+        private T[] _array;
+
+        /// <summary>
+        /// Indicates whether the bag is empty.
+        /// </summary>
+        public bool IsEmpty => (Size == 0);
+
+        /// <summary>
+        /// Returns the number of items in the bag.
+        /// </summary>
+        public int Size { get; private set; }
+
+        public BagRandom(int capacity)
+        {
+            _array = new T[capacity];
+        }
+
+        /// <summary>
+        /// Adds an item to the bag.
+        /// </summary>
+        /// <param name="item">An item to add</param>
+        public void Add(T item)
+        {
+            // Check if there is room in the array. If not, double the size of the array.
+            if (Size == _array.Length)
+                Resize(2 * _array.Length);
+
+            // Add the item to the bag. First, set _array[Size]=item 
+            // and then increment Size.
+            _array[Size++] = item;
+        }
+
+        /// <summary>
+        /// Returns items in the bag in random order. 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator GetEnumerator()
+        {
+            // Randomize the order of the items for this iterator.
+            Shuffle();
+
+            for (int i = 0; i < Size; ++i)
+            {
+                yield return _array[i];
+            }
+        }
+
+        /// <summary>
+        /// Move the _array of size N <= newSize to a new array of size newSize.
+        /// </summary>
+        /// <param name="newSize">The new size of the array</param>
+        private void Resize(int newSize)
+        {
+            T[] tmp = new T[newSize];
+            for (int i = 0; i < Size; ++i)
+                tmp[i] = _array[i];
+            _array = tmp;
+        }
+
+        /// <summary>
+        /// Randomly shuffles the items in the bag's array. 
+        /// </summary>
+        private void Shuffle()
+        {
+            for (int i = 0; i < Size; ++i)
+            {
+                // Exchange a[i] with a random element in a[i..N-1]
+                // All N! permutations are equally likely.
+                // This code is based on [Sedgewick] 1.1 p.36-37
+                int r = _rnd.Next(i, Size);
+                T tmp = _array[i];
+                _array[i] = _array[r];
+                _array[r] = tmp;
+            }
+        }
+    }
+}
--- WbsAlgorithmsTest/Collections/BagTest.cs
+++ WbsAlgorithmsTest/Collections/BagTest.cs
@@ -1,6 +1,7 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using WbsAlgorithms.Collections;
 
 namespace WbsAlgorithmsTest.Collections
@@ -8,6 +9,17 @@ namespace WbsAlgorithmsTest.Collections
     [TestFixture]
     public class BagTest
     {
+        private const int RandomBagSize = 4;
+        private const int RandomBagTestCaseCount = 100000;
+
+        private Dictionary<string, int> _permutations = new Dictionary<string, int>()
+        {
+            {"1234", 0}, {"2134", 0}, {"3124", 0}, {"1324", 0}, {"2314", 0}, {"3214", 0},
+            {"4213", 0}, {"2413", 0}, {"1423", 0}, {"4123", 0}, {"2143", 0}, {"1243", 0},
+            {"1342", 0}, {"3142", 0}, {"4132", 0}, {"1432", 0}, {"3412", 0}, {"4312", 0},
+            {"4321", 0}, {"3421", 0}, {"2431", 0}, {"4231", 0}, {"3241", 0}, {"2341", 0}
+        };
+
         [Test]
         public void AddElementsTest()
         {
@@ -31,6 +43,39 @@ namespace WbsAlgorithmsTest.Collections
             CollectionAssert.AreEqual(
                 elementsToAdd.OrderBy(v => v),
                 elementsInBag.OrderBy(v => v));
+        }
+
+        [Test]
+        public void AllPermutationsShouldBeEquallyLikely()
+        {
+            var bag = new BagRandom<int>(RandomBagSize);
+
+            bag.Add(1);
+            bag.Add(2);
+            bag.Add(3);
+            bag.Add(4);
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < RandomBagTestCaseCount; ++i)
+            {
+                sb.Clear();
+                foreach (var n in bag)
+                    sb.Append(n.ToString());
+
+                _permutations[sb.ToString()]++;
+            }
+
+            // The expected value is TestCaseCount / # of permutations, for example: 
+            // 1000 / 24 = 41.67
+            // 10,000 / 24 = 416.67
+            // 100,000 / 24 = 4166.67
+
+            double expected = (double)RandomBagTestCaseCount / _permutations.Count;
+            double delta = expected * 0.05; // +/- 5%
+            foreach (var p in _permutations)
+            {
+                Assert.AreEqual(expected, p.Value, delta);
+            }
         }
     }
 }
