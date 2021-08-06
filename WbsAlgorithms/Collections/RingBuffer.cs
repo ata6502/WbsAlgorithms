0c362e124add4a11fbaf1b5d172b78b552ad15fe
Leszek Taratuta <ata6502@hotmail.com>
2021-08-05 22:12:42 -0400

    Add a ring buffer data structure

---
+++ WbsAlgorithms/Collections/RingBuffer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace WbsAlgorithms.DataStructs
+{
+    /// <summary>
+    /// A ring buffer (a.k.a. a circular queue) is a FIFO data structure
+    /// useful for transferring data between asynchronous processes or
+    /// for storing log files.
+    /// 
+    /// This implementation uses a fixed-size array with circular 
+    /// wrap-around to store items.
+    /// 
+    /// [Sedgewick] 1.3.39 p.169 - Implement a fixed-size ring buffer with circular wrap-around.
+    /// </summary>
+    public class RingBuffer<T>
+    {
+        private T[] _array; // buffer entries
+        private int _capacity;
+        private bool _isOverflow = false;
+
+        private int _first = -1; // the index of the first item
+        private int _last = -1; // the index of the last item
+
+        /// <summary>
+        /// Indicates whether the buffer is empty.
+        /// </summary>
+        public bool IsEmpty => Size == 0;
+
+        /// <summary>
+        /// Returns the number of items in the buffer.
+        /// </summary>
+        public int Size { get; private set; }
+
+        public RingBuffer(int capacity)
+        {
+            _capacity = capacity;
+            _array = new T[capacity];
+        }
+
+        /// <summary>
+        /// Adds an item to the buffer.
+        /// </summary>
+        /// <param name="item">An item to add</param>
+        public void Put(T item)
+        {
+            ++_last;
+
+            // Wrap-around if the buffer capacity has been exceeded.
+            if (_last >= _capacity)
+            {
+                _last = 0;
+                _isOverflow = true;
+            }
+
+            _array[_last] = item;
+
+            if (_isOverflow)
+                _first = _last;
+
+            Size = Math.Min(Size+1, _capacity);
+        }
+
+        /// <summary>
+        /// Removes and returns an item from the buffer. Throws an exception if the buffer is empty.
+        /// </summary>
+        /// <returns>An item removed from the buffer</returns>
+        public T Get()
+        {
+            if (Size == 0)
+                throw new IndexOutOfRangeException("Buffer is empty");
+
+            ++_first;
+
+            // wrap-around
+            if (_first >= _capacity)
+                _first = 0;
+
+            T item = _array[_first];
+
+            // Avoid loitering by overwriting the orphaned reference (applicable to reference types).
+            _array[_first] = default(T); 
+
+            _isOverflow = false;
+            --Size;
+
+            return item;
+        }
+
+        public override string ToString()
+        {
+            return string.Join(' ', _array);
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Collections/RingBufferTest.cs
@@ -0,0 +1,126 @@
+﻿using NUnit.Framework;
+using System;
+using WbsAlgorithms.DataStructs;
+
+namespace WbsAlgorithmsTest.Collections
+{
+    [TestFixture]
+    class RingBufferTest
+    {
+        [Test]
+        public void EmptyBufferTest()
+        {
+            var buffer = new RingBuffer<int>(3);
+
+            Assert.IsTrue(buffer.IsEmpty);
+            Assert.AreEqual(0, buffer.Size);
+
+            // If the buffer is empty, it should throw an exception.
+            Assert.Throws<IndexOutOfRangeException>(() => buffer.Get());
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            var buffer = new RingBuffer<int>(3);
+
+            buffer.Put(1);
+            Assert.AreEqual(1, buffer.Get());
+            Assert.IsTrue(buffer.IsEmpty);
+
+            buffer.Put(1);
+            buffer.Put(2);
+            Assert.AreEqual(1, buffer.Get());
+            Assert.AreEqual(2, buffer.Get());
+            Assert.IsTrue(buffer.IsEmpty);
+
+            buffer.Put(1);
+            buffer.Put(2);
+            buffer.Put(3);
+            Assert.AreEqual(1, buffer.Get());
+            Assert.AreEqual(2, buffer.Get());
+            Assert.AreEqual(3, buffer.Get());
+            Assert.IsTrue(buffer.IsEmpty);
+        }
+
+        [Test]
+        public void OverflowTest()
+        {
+            var buffer = new RingBuffer<int>(3);
+
+            buffer.Put(1);
+            buffer.Put(2);
+            buffer.Put(3);
+            buffer.Put(4); // overrides 1
+            Assert.AreEqual(2, buffer.Get());
+            Assert.AreEqual(3, buffer.Get());
+            Assert.AreEqual(4, buffer.Get());
+            Assert.IsTrue(buffer.IsEmpty);
+
+            buffer.Put(1);
+            buffer.Put(2);
+            buffer.Put(3);
+            buffer.Put(4); // overrides 1 
+            buffer.Put(5); // overrides 2
+            Assert.AreEqual(3, buffer.Get());
+            Assert.AreEqual(4, buffer.Get());
+            Assert.AreEqual(5, buffer.Get());
+            Assert.IsTrue(buffer.IsEmpty);
+
+            buffer.Put(1);
+            buffer.Put(2);
+            buffer.Put(3);
+            buffer.Put(4); // overrides 1 
+            buffer.Put(5); // overrides 2
+            buffer.Put(6); // overrides 3 
+            Assert.AreEqual(4, buffer.Get());
+            Assert.AreEqual(5, buffer.Get());
+            Assert.AreEqual(6, buffer.Get());
+            Assert.IsTrue(buffer.IsEmpty);
+
+            buffer.Put(1);
+            buffer.Put(2);
+            buffer.Put(3);
+            buffer.Put(4); // overrides 1 
+            buffer.Put(5); // overrides 2
+            buffer.Put(6); // overrides 3 
+            buffer.Put(7); // overrides 4
+            Assert.AreEqual(5, buffer.Get());
+            Assert.AreEqual(6, buffer.Get());
+            Assert.AreEqual(7, buffer.Get());
+            Assert.IsTrue(buffer.IsEmpty);
+        }
+
+        [Test]
+        public void MixedTest()
+        {
+            var buffer = new RingBuffer<int>(3);
+
+            buffer.Put(1);
+            buffer.Put(2);
+            buffer.Put(3);
+            buffer.Put(4); // overrides 1
+            
+            Assert.AreEqual(2, buffer.Get());
+
+            buffer.Put(5);
+
+            Assert.AreEqual(3, buffer.Get());
+            Assert.AreEqual(4, buffer.Get());
+
+            buffer.Put(6);
+            buffer.Put(7);
+
+            Assert.AreEqual(5, buffer.Get());
+            Assert.AreEqual(6, buffer.Get());
+
+            buffer.Put(8);
+
+            Assert.AreEqual(7, buffer.Get());
+            Assert.AreEqual(8, buffer.Get());
+
+            Assert.IsTrue(buffer.IsEmpty);
+
+        }
+    }
+}
