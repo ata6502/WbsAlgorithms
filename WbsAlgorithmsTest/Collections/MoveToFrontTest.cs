f266bed2253938f0b3b69bbe059e68d0161352a6
Leszek Taratuta <ata6502@hotmail.com>
2021-08-06 21:59:35 -0400

    Add text editor buffer data structure

---
+++ WbsAlgorithmsTest/Collections/MoveToFrontTest.cs
@@ -0,0 +1,198 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.Collections;
+
+namespace WbsAlgorithmsTest.Collections
+{
+    [TestFixture]
+    class MoveToFrontTest
+    {
+        [Test]
+        public void OneElementTest()
+        {
+            var m = new MoveToFront<int>();
+
+            m.Add(1);
+
+            Assert.AreEqual(1, m.Head.Item);
+            Assert.IsNull(m.Head.Next);
+
+            m.Add(1);
+
+            Assert.AreEqual(1, m.Head.Item);
+            Assert.IsNull(m.Head.Next);
+        }
+
+        [Test]
+        public void TwoElementsTest()
+        {
+            var m = new MoveToFront<int>();
+
+            m.Add(1);
+
+            Assert.AreEqual(1, m.Head.Item);
+            Assert.IsNull(m.Head.Next);
+
+            m.Add(2);
+
+            Assert.AreEqual(2, m.Head.Item);
+            Assert.AreEqual(1, m.Head.Next.Item);
+            Assert.IsNull(m.Head.Next.Next);
+
+            m.Add(1);
+
+            Assert.AreEqual(1, m.Head.Item);
+            Assert.AreEqual(2, m.Head.Next.Item);
+            Assert.IsNull(m.Head.Next.Next);
+
+            m.Add(2);
+
+            Assert.AreEqual(2, m.Head.Item);
+            Assert.AreEqual(1, m.Head.Next.Item);
+            Assert.IsNull(m.Head.Next.Next);
+
+            m.Add(2);
+
+            Assert.AreEqual(2, m.Head.Item);
+            Assert.AreEqual(1, m.Head.Next.Item);
+            Assert.IsNull(m.Head.Next.Next);
+
+            m.Add(1);
+
+            Assert.AreEqual(1, m.Head.Item);
+            Assert.AreEqual(2, m.Head.Next.Item);
+            Assert.IsNull(m.Head.Next.Next);
+
+            m.Add(1);
+
+            Assert.AreEqual(1, m.Head.Item);
+            Assert.AreEqual(2, m.Head.Next.Item);
+            Assert.IsNull(m.Head.Next.Next);
+        }
+
+        [Test]
+        public void ThreeElementsTest()
+        {
+            var m = new MoveToFront<int>();
+
+            m.Add(1);
+            m.Add(2);
+            m.Add(3);
+            m.Add(1);
+
+            Assert.AreEqual(1, m.Head.Item);
+            Assert.AreEqual(3, m.Head.Next.Item);
+            Assert.AreEqual(2, m.Head.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next);
+
+            m.Add(3);
+
+            Assert.AreEqual(3, m.Head.Item);
+            Assert.AreEqual(1, m.Head.Next.Item);
+            Assert.AreEqual(2, m.Head.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next);
+
+            m.Add(3);
+
+            Assert.AreEqual(3, m.Head.Item);
+            Assert.AreEqual(1, m.Head.Next.Item);
+            Assert.AreEqual(2, m.Head.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next);
+        }
+
+        [Test]
+        public void MixedTest()
+        {
+            var m = new MoveToFront<char>();
+
+            m.Add('A');
+            Assert.AreEqual('A', m.Head.Item);
+            Assert.IsNull(m.Head.Next);
+
+            m.Add('B');
+            Assert.AreEqual('B', m.Head.Item);
+            Assert.AreEqual('A', m.Head.Next.Item);
+            Assert.IsNull(m.Head.Next.Next);
+
+            m.Add('C');
+            Assert.AreEqual('C', m.Head.Item);
+            Assert.AreEqual('B', m.Head.Next.Item);
+            Assert.AreEqual('A', m.Head.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next);
+
+            m.Add('C');
+            Assert.AreEqual('C', m.Head.Item);
+            Assert.AreEqual('B', m.Head.Next.Item);
+            Assert.AreEqual('A', m.Head.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next);
+
+            m.Add('D');
+            Assert.AreEqual('D', m.Head.Item);
+            Assert.AreEqual('C', m.Head.Next.Item);
+            Assert.AreEqual('B', m.Head.Next.Next.Item);
+            Assert.AreEqual('A', m.Head.Next.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next.Next);
+
+            m.Add('A');
+            Assert.AreEqual('A', m.Head.Item);
+            Assert.AreEqual('D', m.Head.Next.Item);
+            Assert.AreEqual('C', m.Head.Next.Next.Item);
+            Assert.AreEqual('B', m.Head.Next.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next.Next);
+
+            m.Add('B');
+            Assert.AreEqual('B', m.Head.Item);
+            Assert.AreEqual('A', m.Head.Next.Item);
+            Assert.AreEqual('D', m.Head.Next.Next.Item);
+            Assert.AreEqual('C', m.Head.Next.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next.Next);
+
+            m.Add('E');
+            Assert.AreEqual('E', m.Head.Item);
+            Assert.AreEqual('B', m.Head.Next.Item);
+            Assert.AreEqual('A', m.Head.Next.Next.Item);
+            Assert.AreEqual('D', m.Head.Next.Next.Next.Item);
+            Assert.AreEqual('C', m.Head.Next.Next.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next.Next.Next);
+
+            m.Add('B');
+            Assert.AreEqual('B', m.Head.Item);
+            Assert.AreEqual('E', m.Head.Next.Item);
+            Assert.AreEqual('A', m.Head.Next.Next.Item);
+            Assert.AreEqual('D', m.Head.Next.Next.Next.Item);
+            Assert.AreEqual('C', m.Head.Next.Next.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next.Next.Next);
+
+            m.Add('A');
+            Assert.AreEqual('A', m.Head.Item);
+            Assert.AreEqual('B', m.Head.Next.Item);
+            Assert.AreEqual('E', m.Head.Next.Next.Item);
+            Assert.AreEqual('D', m.Head.Next.Next.Next.Item);
+            Assert.AreEqual('C', m.Head.Next.Next.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next.Next.Next);
+
+            m.Add('C');
+            Assert.AreEqual('C', m.Head.Item);
+            Assert.AreEqual('A', m.Head.Next.Item);
+            Assert.AreEqual('B', m.Head.Next.Next.Item);
+            Assert.AreEqual('E', m.Head.Next.Next.Next.Item);
+            Assert.AreEqual('D', m.Head.Next.Next.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next.Next.Next);
+
+            m.Add('A');
+            Assert.AreEqual('A', m.Head.Item);
+            Assert.AreEqual('C', m.Head.Next.Item);
+            Assert.AreEqual('B', m.Head.Next.Next.Item);
+            Assert.AreEqual('E', m.Head.Next.Next.Next.Item);
+            Assert.AreEqual('D', m.Head.Next.Next.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next.Next.Next);
+
+            m.Add('A');
+            Assert.AreEqual('A', m.Head.Item);
+            Assert.AreEqual('C', m.Head.Next.Item);
+            Assert.AreEqual('B', m.Head.Next.Next.Item);
+            Assert.AreEqual('E', m.Head.Next.Next.Next.Item);
+            Assert.AreEqual('D', m.Head.Next.Next.Next.Next.Item);
+            Assert.IsNull(m.Head.Next.Next.Next.Next.Next);
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Collections/TextEditorBufferTest.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.Collections;
+
+namespace WbsAlgorithmsTest.Collections
+{
+    [TestFixture]
+    public class TextEditorBufferTest
+    {
+        [Test]
+        public void MixedTest()
+        {
+            var b = new TextEditorBuffer();
+            b.Insert('A');
+            b.Insert('B');
+            b.Insert('C');
+            b.Insert('D');
+            b.Insert('E');
+            b.Insert('F');
+            Assert.AreEqual("A B C D E F | ", b.ToString());
+
+            b.Left(4);
+            Assert.AreEqual("A B | C D E F", b.ToString());
+
+            b.Right(3);
+            Assert.AreEqual("A B C D E | F", b.ToString());
+
+            b.Right(1);
+            Assert.AreEqual("A B C D E F | ", b.ToString());
+
+            b.Right(1); // try to move cursor beyond the last element
+            Assert.AreEqual("A B C D E F | ", b.ToString());
+
+            b.Left(3);
+            Assert.AreEqual("A B C | D E F", b.ToString());
+
+            b.Right(10); // try to move cursor beyond the last element
+            Assert.AreEqual("A B C D E F | ", b.ToString());
+
+            b.Left(4);
+            Assert.AreEqual("A B | C D E F", b.ToString());
+
+            b.Left(1);
+            Assert.AreEqual("A | B C D E F", b.ToString());
+
+            b.Left(1);
+            Assert.AreEqual(" | A B C D E F", b.ToString());
+
+            b.Left(1); // try to move cursor before the first element
+            Assert.AreEqual(" | A B C D E F", b.ToString());
+
+            b.Right(3);
+            Assert.AreEqual("A B C | D E F", b.ToString());
+
+            b.Left(15); // try to move cursor before the first element
+            Assert.AreEqual(" | A B C D E F", b.ToString());
+            Assert.AreEqual(b.Delete(), default(char));
+
+            b.Right(2);
+            Assert.AreEqual("A B | C D E F", b.ToString());
+            Assert.AreEqual(b.Delete(), 'B');
+            Assert.AreEqual("A | C D E F", b.ToString());
+            Assert.AreEqual(b.Get(), 'A');
+            Assert.AreEqual("A | C D E F", b.ToString());
+
+            b.Right(1);
+            Assert.AreEqual("A C | D E F", b.ToString());
+            Assert.AreEqual(b.Get(), 'C');
+
+            b.Left(10);
+            Assert.AreEqual(" | A C D E F", b.ToString());
+            Assert.AreEqual(b.Get(), default(char));
+            Assert.AreEqual(b.Delete(), default(char));
+
+            b.Right(10);
+            Assert.AreEqual("A C D E F | ", b.ToString());
+            Assert.AreEqual(b.Get(), 'F');
+            Assert.AreEqual(b.Delete(), 'F');
+            Assert.AreEqual("A C D E | ", b.ToString());
+            Assert.AreEqual(b.Delete(), 'E');
+            Assert.AreEqual("A C D | ", b.ToString());
+            Assert.AreEqual(b.Delete(), 'D');
+            Assert.AreEqual("A C | ", b.ToString());
+            Assert.AreEqual(b.Delete(), 'C');
+            Assert.AreEqual("A | ", b.ToString());
+            Assert.AreEqual(b.Delete(), 'A');
+            Assert.AreEqual(" | ", b.ToString());
+            Assert.AreEqual(0, b.Size);
+        }
+    }
+}
