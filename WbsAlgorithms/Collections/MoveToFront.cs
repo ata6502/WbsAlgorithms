f282952e90735b70947fb4f3f5f2482ff6d8734f
Leszek Taratuta <ata6502@hotmail.com>
2021-08-06 21:57:21 -0400

    Add move-to-front strategy

---
+++ WbsAlgorithms/Collections/MoveToFront.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using WbsAlgorithms.Common;
+
+namespace WbsAlgorithms.Collections
+{
+    /// <summary>
+    /// The move-to-front strategy maintains a linked list with no duplicates:
+    /// - When a previously unseen element is read, it is inserted at the front of the list.
+    /// - When a duplicate element is read, it is deleted from the list and reinserted at the front of the list.
+    ///
+    /// The move-to-front strategy is useful for:
+    /// - caching
+    /// - data compression
+    /// - wherever items that have been recently accessed are more likely to be reaccessed
+    /// 
+    /// [Sedgewick] 1.3.40 p.169 - Implement move-to-front strategy that keeps items in a linked list.
+    /// </summary>
+    public class MoveToFront<T> where T : IComparable<T>
+    {
+        private ListNode<T> _head; // the front of the list
+
+        /// <summary>
+        /// Returns a pointer to the head of the linked list. It's used
+        /// for testing the move-to-front strategy.
+        /// </summary>
+        public ListNode<T> Head => _head;
+
+        /// <summary>
+        /// Adds an item according to the move-to-front strategy.
+        /// </summary>
+        /// <param name="item">An item do add</param>
+        public void Add(T item)
+        {
+            // If the list is empty, add the new item as the list's head.
+            if (_head == null)
+            {
+                _head = new ListNode<T>(item);
+                return;
+            }
+
+            // Try to find a node with the value 'item'. Also, keep the node previous to the found one.
+            ListNode<T> prev = null;
+            ListNode<T> node = _head;
+
+            while (node != null && node.Item.CompareTo(item) != 0)
+            {
+                prev = node;
+                node = node.Next;
+            }
+
+            var isItemFound = node != null && node.Item.CompareTo(item) == 0;
+
+            // If the node already exists in the list, remove it.
+            if (isItemFound)
+            {
+                if (prev == null)
+                    // The only node that does not have a previous node is the head. Remove the head.
+                    _head = node.Next; 
+                else
+                    // Remove the node.
+                    prev.Next = node.Next; 
+            }
+
+            // Inserted or re-insert (if it was a duplicate) the item at the front of the list
+            _head = new ListNode<T>(item, _head);
+        }
+    }
+}
---
+++ WbsAlgorithms/Collections/TextEditorBuffer.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace WbsAlgorithms.Collections
+{
+    /// <summary>
+    /// [Sedgewick] 1.3.44 p.170 - Develop a data type for a buffer in 
+    /// a text editor that implements the following API:
+    /// 
+    /// int  Size()          // return the number of characters in the buffer
+    /// void Insert(char c)  // insert a character c at the cursor position
+    /// char Get()           // get the character at the cursor position
+    /// char Delete()        // delete and return the character at the cursor position
+    /// void Left(int k)     // move the cursor k positions to the left
+    /// void Right(int k)    // move the cursor k positions to the right
+    /// </summary>
+    public class TextEditorBuffer
+    {
+        // Stores the characters on the left from the cursor position.
+        // The top of the _left stack refers to the current cursor position.
+        private StackLinkedList<char> _left;
+
+        // Stores the character on the right from the cursor position.
+        // The top of the _right stack refers to the first character on the right from the cursor.
+        private StackLinkedList<char> _right;
+
+        /// <summary>
+        /// Returns the number of characters in the buffer.
+        /// </summary>
+        public int Size => _left.Size + _right.Size;
+
+        public TextEditorBuffer()
+        {
+            _left = new StackLinkedList<char>();
+            _right = new StackLinkedList<char>();
+        }
+
+        /// <summary>
+        /// Inserts a character at the cursor position.
+        /// </summary>
+        /// <param name="c">The character to insert</param>
+        public void Insert(char c)
+        {
+            _left.Push(c);
+        }
+
+        /// <summary>
+        /// Gets the character at the cursor position.
+        /// </summary>
+        /// <returns>The character at the cursor position. Returns '\0' if the buffer is empty.</returns>
+        public char Get()
+        {
+            if (_left.Size == 0)
+                return default(char);
+
+            return _left.Peek();
+        }
+
+        /// <summary>
+        /// Deletes and returns the character at the cursor position.
+        /// </summary>
+        /// <returns>The character at the cursor position. Returns '\0' if the cursor is at the beginning of the buffer.</returns>
+        public char Delete()
+        {
+            if (_left.Size == 0)
+                return default(char);
+
+            return _left.Pop();
+        }
+
+        /// <summary>
+        /// Move the cursor k positions to the left.
+        /// </summary>
+        /// <param name="k">The number of positions to move the cursor</param>
+        public void Left(int k)
+        {
+            if (Size == 0)
+                return;
+
+            // Determine the number of positions the cursor has to be moved.
+            // Take into account that there may be not enough characters on
+            // the left to move the cursor.
+            var n = Math.Min(_left.Size, k);
+
+            // Move the cursor to the left.
+            for (var i = 0; i < n; ++i)
+                _right.Push(_left.Pop());
+        }
+
+        public void Right(int k)
+        {
+            if (Size == 0)
+                return;
+
+            // Determine the number of positions the cursor has to be moved.
+            // Take into account that there may be not enough characters on
+            // the right to move the cursor.
+            var n = Math.Min(_right.Size, k);
+
+            // Move the cursor to the right.
+            for (var i = 0; i < n; ++i)
+                _left.Push(_right.Pop());
+        }
+
+        /// <summary>
+        /// Shows the characters in the buffer. The character '|' marks the current
+        /// position of the cursot.
+        /// </summary>
+        /// <returns>Formatted content of the buffer</returns>
+        public override string ToString()
+        {
+            return string.Join(' ', ReverseString(_left.ToString())) + " | " + string.Join(' ', _right.ToString());
+        }
+
+        private string ReverseString(string s)
+        {
+            char[] a = s.ToCharArray();
+            Array.Reverse(a);
+            return new string(a);
+        }
+    }
+}
