36e016a8252f54c883fcf26c29a08e4088df910f
Leszek Taratuta <ata6502@hotmail.com>
2021-08-09 21:20:36 -0400

    Add DoubleStack data structure

---
+++ WbsAlgorithms/Collections/DoubleStack.cs
@@ -0,0 +1,75 @@
+﻿namespace WbsAlgorithms.Collections
+{
+    /// <summary>
+    /// [Sedgewick] 1.3.48 p.171 - Implementat two stacks using a single deque. 
+    /// Each stack operation takes a constant number of deque operations.
+    /// </summary>
+    public class DoubleStack<T>
+    {
+        private DequeLinkedList<T> d = new DequeLinkedList<T>();
+
+        /// <summary>
+        /// Indicates whether the left stack is empty.
+        /// </summary>
+        public bool IsLeftEmpty { get { return LeftSize == 0; } }
+
+        /// <summary>
+        /// The size of the left stack.
+        /// </summary>
+        public int LeftSize { get; private set; }
+
+        /// <summary>
+        /// Indicates whether the right stack is empty.
+        /// </summary>
+        public bool IsRightEmpty { get { return RightSize == 0; } }
+
+        /// <summary>
+        /// The size of the right stack.
+        /// </summary>
+        public int RightSize { get; private set; }
+
+        /// <summary>
+        /// Adds an item to the left stack.
+        /// </summary>
+        /// <param name="item">An item to add</param>
+        public void PushLeft(T item)
+        {
+            d.PushLeft(item);
+            ++LeftSize;
+        }
+
+        /// <summary>
+        /// Adds an item to the right stack.
+        /// </summary>
+        /// <param name="item">An item to add</param>
+        public void PushRight(T item)
+        {
+            d.PushRight(item);
+            ++RightSize;
+        }
+
+        /// <summary>
+        /// Removes an item from the left stack and returns it.
+        /// </summary>
+        /// <returns>The removed item</returns>
+        public T PopLeft()
+        {
+            // Deque.PopLeft throws an exception if the deque is empty.
+            T item = d.PopLeft();
+            --LeftSize;
+            return item;
+        }
+
+        /// <summary>
+        /// Removes an item from the right stack and returns it.
+        /// </summary>
+        /// <returns>The removed item</returns>
+        public T PopRight()
+        {
+            // Deque.PopRight throws an exception if the deque is empty.
+            T item = d.PopRight();
+            --RightSize;
+            return item;
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Collections/DoubleStackTest.cs
@@ -0,0 +1,106 @@
+﻿using NUnit.Framework;
+using System;
+using WbsAlgorithms.Collections;
+
+namespace WbsAlgorithmsTest.Collections
+{
+    [TestFixture]
+    class DoubleStackTest
+    {
+        [Test]
+        public void EmptyDoubleStackTest()
+        {
+            var ds = new DoubleStack<int>();
+
+            Assert.IsTrue(ds.IsLeftEmpty);
+            Assert.AreEqual(0, ds.LeftSize);
+
+            Assert.IsTrue(ds.IsRightEmpty);
+            Assert.AreEqual(0, ds.RightSize);
+
+            Assert.Throws<ArgumentException>(() => ds.PopLeft());
+            Assert.Throws<ArgumentException>(() => ds.PopRight());
+        }
+
+        [Test]
+        public void OneElementDoubleStackTest()
+        {
+            var ds = new DoubleStack<int>();
+
+            ds.PushRight(1);
+            Assert.IsFalse(ds.IsRightEmpty);
+            Assert.IsTrue(ds.IsLeftEmpty);
+            Assert.AreEqual(1, ds.PopRight());
+            Assert.IsTrue(ds.IsRightEmpty);
+
+            ds.PushLeft(1);
+            Assert.IsFalse(ds.IsLeftEmpty);
+            Assert.IsTrue(ds.IsRightEmpty);
+            Assert.AreEqual(1, ds.PopLeft());
+            Assert.IsTrue(ds.IsLeftEmpty);
+        }
+
+        [Test]
+        public void TwoElementsDoubleStackTest()
+        {
+            var ds = new DoubleStack<int>();
+
+            ds.PushLeft(1);
+            ds.PushRight(2);
+            Assert.AreEqual(1, ds.LeftSize);
+            Assert.AreEqual(1, ds.RightSize);
+
+            Assert.AreEqual(1, ds.PopLeft());
+            Assert.AreEqual(2, ds.PopRight());
+        }
+
+        [Test]
+        public void ThreeElementsDoubleStackTest()
+        {
+            var ds = new DoubleStack<int>();
+
+            ds.PushLeft(2);
+            ds.PushRight(3);
+            ds.PushLeft(1);
+            Assert.AreEqual(2, ds.LeftSize);
+            Assert.AreEqual(1, ds.RightSize);
+
+            Assert.AreEqual(1, ds.PopLeft());
+            Assert.AreEqual(3, ds.PopRight());
+            Assert.AreEqual(2, ds.PopLeft());
+        }
+
+        [Test]
+        public void MixedElementsDoubleStackTest()
+        {
+            var ds = new DoubleStack<int>();
+
+            ds.PushLeft(3);
+            ds.PushLeft(2);
+            ds.PushLeft(1);
+            Assert.AreEqual(3, ds.LeftSize);
+
+            Assert.AreEqual(1, ds.PopLeft());
+            Assert.AreEqual(2, ds.LeftSize);
+
+            Assert.AreEqual(2, ds.PopLeft());
+            Assert.AreEqual(1, ds.LeftSize);
+
+            ds.PushRight(4);
+            ds.PushRight(5);
+            Assert.AreEqual(2, ds.RightSize);
+
+            Assert.AreEqual(3, ds.PopLeft());
+            Assert.AreEqual(0, ds.LeftSize);
+
+            Assert.AreEqual(5, ds.PopRight());
+            Assert.AreEqual(1, ds.RightSize);
+
+            Assert.AreEqual(4, ds.PopRight());
+            Assert.AreEqual(0, ds.RightSize);
+
+            Assert.IsTrue(ds.IsLeftEmpty);
+            Assert.IsTrue(ds.IsRightEmpty);
+        }
+    }
+}
