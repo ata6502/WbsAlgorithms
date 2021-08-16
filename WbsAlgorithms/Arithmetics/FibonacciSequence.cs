6d100f9183adf6587ea5365bc4d82fb6f74a4353
Leszek Taratuta <ata6502@hotmail.com>
2021-08-16 15:44:51 -0400

    Add Fibonacci sequence generator

---
+++ WbsAlgorithms/Arithmetics/FibonacciSequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace WbsAlgorithms.Arithmetics
+{
+    public class FibonacciSequence
+    {
+        /// <summary>
+        /// [Sedgewick] 1.1.19 p.57 - Compute the Fibonacci sequence by saving computed values 
+        /// in an array.
+        /// 
+        /// Examples:
+        /// ComputeSequenceUsingArray(5) returns { 0 1 1 2 3 5 }
+        /// ComputeSequenceUsingArray(13) returns { 0 1 1 2 3 5 8 13 21 34 55 89 144 233 }
+        /// </summary>
+        /// <param name="n">An input number from 0 to 100 for which to compute the Fibonacci sequence</param>
+        /// <returns>The Fibonacci sequence for numbers from 0 to n</returns>
+        public static long[] ComputeSequenceUsingArray(int n)
+        {
+            const int MaxNumber = 100;
+
+            if (n > MaxNumber)
+                throw new ArgumentOutOfRangeException($"The input number {n} is too large for this implementation of Fibonacci sequence.");
+
+            if (n == 0)
+                return new long[] { 0 };
+            if (n == 1)
+                return new long[] { 0, 1 };
+
+            var a = new long[n+1];
+            a[0] = 0;
+            a[1] = 1;
+
+            for (var i = 2; i < a.Length; ++i)
+                a[i] = a[i - 1] + a[i - 2];
+
+            return a;
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Arithmetics/FibonacciSequenceTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.Arithmetics;
+
+namespace WbsAlgorithmsTest.Arithmetics
+{
+    [TestFixture]
+    public class FibonacciSequenceTest
+    {
+        [TestCase(0, "0")]
+        [TestCase(1, "0 1")]
+        [TestCase(2, "0 1 1")]
+        [TestCase(3, "0 1 1 2")]
+        [TestCase(4, "0 1 1 2 3")]
+        [TestCase(5, "0 1 1 2 3 5")]
+        [TestCase(6, "0 1 1 2 3 5 8")]
+        [TestCase(7, "0 1 1 2 3 5 8 13")]
+        [TestCase(8, "0 1 1 2 3 5 8 13 21")]
+        [TestCase(9, "0 1 1 2 3 5 8 13 21 34")]
+        [TestCase(10, "0 1 1 2 3 5 8 13 21 34 55")]
+        [TestCase(11, "0 1 1 2 3 5 8 13 21 34 55 89")]
+        [TestCase(12, "0 1 1 2 3 5 8 13 21 34 55 89 144")]
+        [TestCase(13, "0 1 1 2 3 5 8 13 21 34 55 89 144 233")]
+        [TestCase(14, "0 1 1 2 3 5 8 13 21 34 55 89 144 233 377")]
+        [TestCase(15, "0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610")]
+        [TestCase(16, "0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987")]
+        [TestCase(17, "0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597")]
+        [TestCase(18, "0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584")]
+        [TestCase(19, "0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181")]
+        [TestCase(20, "0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765")]
+        public void ComputeFibonacciNumberTest(int inputNumber, string expectedSequence)
+        {
+            var actualSequence = string.Join(' ', FibonacciSequence.ComputeSequenceUsingArray(inputNumber));
+
+            Assert.AreEqual(expectedSequence, actualSequence);
+        }
+    }
+}
