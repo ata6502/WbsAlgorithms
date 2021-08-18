7db8e0663413099ffbfe6408ee5b9dd891204796
Leszek Taratuta <ata6502@hotmail.com>
2021-08-17 21:52:46 -0400

    Add logarithm of factorial calculations

---
+++ WbsAlgorithms/Arithmetics/LogOfFactorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WbsAlgorithms.Arithmetics
+{
+    /// <summary>
+    /// [Sedgewick] 1.1.20 p.58 - Write a recursive method that computes ln(n!)
+    /// By convention for an empty product: 0! = 1
+    /// </summary>
+    public class LogOfFactorial
+    {
+        public static double Compute(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("The input value should be greater of equal zero.");
+
+            // By the log rule: log(a·b) = log(a) + log(b)
+            return ComputeRecursively(n);
+        }
+
+        private static double ComputeRecursively(int n)
+        {
+            if (n == 0 || n == 1)
+                return Math.Log(1);
+
+            return Math.Log(n) + ComputeRecursively(n - 1);
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Arithmetics/LogOfFactorialTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using WbsAlgorithms.Arithmetics;
+
+namespace WbsAlgorithmsTest.Arithmetics
+{
+    [TestFixture]
+    public class LogOfFactorialTest
+    {
+        [TestCase(0, 0)]
+        [TestCase(1, 0)]
+        [TestCase(2, 0.69314718055995)]
+        [TestCase(3, 1.79175946922806)]
+        [TestCase(4, 3.17805383034795)]
+        [TestCase(5, 4.78749174278205)]
+        [TestCase(6, 6.57925121201010)]
+        [TestCase(7, 8.52516136106541)]
+        [TestCase(8, 10.60460290274525)]
+        [TestCase(9, 12.80182748008147)]
+        [TestCase(10, 15.10441257307552)]
+        public void ComputeLogOfFactorialTest(int inputNumber, double expectedValue)
+        {
+            var actualValue = LogOfFactorial.Compute(inputNumber);
+
+            Assert.AreEqual(expectedValue, actualValue, 0.00000000000001);
+        }
+
+        [Test]
+        public void InvalidArgumentTest()
+        {
+            Assert.Throws<ArgumentException>(() => LogOfFactorial.Compute(-1));
+        }
+    }
+}
