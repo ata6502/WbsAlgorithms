acf47c614f756abbbf08a0e8fa65f9a52cb1ec2d
Leszek Taratuta <ata6502@hotmail.com>
2021-08-18 21:16:00 -0400

    Sort three numbers

---
+++ WbsAlgorithms/Sorting/SortThreeNumbers.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace WbsAlgorithms.Sorting
+{
+    /// <summary>
+    /// [Sedgewick] 1.1.26 p.59 - Sort three numbers.
+    /// </summary>
+    public class SortThreeNumbers
+    {
+        /// <summary>
+        /// Sorts the input array in ascending order. The array is sorted in-place.
+        /// </summary>
+        /// <param name="a">A three-element array.</param>
+        public static void SortAscending(int[] a)
+        {
+            Debug.Assert(a.Length == 3);
+
+            if (a[0] > a[1])
+            {
+                var tmp = a[0];
+                a[0] = a[1];
+                a[1] = tmp;
+            }
+
+            if (a[0] > a[2])
+            {
+                var tmp = a[0];
+                a[0] = a[2];
+                a[2] = tmp;
+            }
+
+            if (a[1] > a[2])
+            {
+                var tmp = a[1];
+                a[1] = a[2];
+                a[2] = tmp;
+            }
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Sorting/SortThreeNumbersTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.Sorting;
+
+namespace WbsAlgorithmsTest.Sorting
+{
+    [TestFixture]
+    public class SortThreeNumbersTest
+    {
+        [TestCase(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 }, TestName = "Test123")]
+        [TestCase(new int[] { 1, 3, 2 }, new int[] { 1, 2, 3 }, TestName = "Test132")]
+        [TestCase(new int[] { 2, 1, 3 }, new int[] { 1, 2, 3 }, TestName = "Test213")]
+        [TestCase(new int[] { 2, 3, 1 }, new int[] { 1, 2, 3 }, TestName = "Test231")]
+        [TestCase(new int[] { 3, 1, 2 }, new int[] { 1, 2, 3 }, TestName = "Test312")]
+        [TestCase(new int[] { 3, 2, 1 }, new int[] { 1, 2, 3 }, TestName = "Test321")]
+        public void SortAscendingTest(int[] inputArray, int[] expectedResult)
+        {
+            // Sorts the input array in-place.
+            SortThreeNumbers.SortAscending(inputArray);
+
+            CollectionAssert.AreEqual(expectedResult, inputArray);
+        }
+    }
+}
