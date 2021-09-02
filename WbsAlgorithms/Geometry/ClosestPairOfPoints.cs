f37491640b782994cb6dbb6a1a45fc058d5dd6cb
Leszek Taratuta <ata6502@hotmail.com>
2021-09-01 21:14:20 -0400

    Add more test cases to closest pair of points

--- WbsAlgorithms/Geometry/ClosestPairOfPoints.cs
+++ WbsAlgorithms/Geometry/ClosestPairOfPoints.cs
@@ -1,33 +1,43 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace WbsAlgorithms.Geometry
 {
     /// <summary>
     /// [Sedgewick] 1.2.1 p.114 - Compute the distance separating the closest pair of points.
     /// </summary>
-    public class ClosestPoints
+    public class ClosestPairOfPoints
     {
         /// <summary>
         /// Computes the distance separating the closest pair of points by iterating through 
         /// all the pairs. Time complexity: O(n^2)
         /// </summary>
         /// <param name="points">A set of 2D points</param>
-        /// <returns>The distance separating the closest pair of points</returns>
-        public static double GetShortestDistanceBruteForce(Point[] points)
+        /// <returns>The distance separating the closest pair of points and the pair of points itself</returns>
+        public static (double Distance, Point[] ClosestPair) ComputeBruteForce(Point[] points)
         {
+            Debug.Assert(points.Length >= 2);
+
             var min = double.MaxValue;
+            var closestPair = new Point[] { points[0], points[1] };
 
             for (int i = 0; i < points.Length; ++i)
             {
                 for (var j = i + 1; j < points.Length; ++j)
                 {
                     var d = GetSquaredDistance(points[i], points[j]);
-                    min = Math.Min(d, min);
+
+                    if (d < min)
+                    {
+                        min = d;
+                        closestPair[0] = points[i];
+                        closestPair[1] = points[j];
+                    }
                 }
             }
 
-            // Caclulate the actual distance as min contains the square of the minimum distance.
-            return Math.Sqrt(min);
+            // Calculate the actual distance as min contains the square of the minimum distance.
+            return (Math.Sqrt(min), closestPair);
         }
 
         /// <summary>
---
+++ WbsAlgorithmsTest/Geometry/ClosestPairOfPointsTest.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Diagnostics;
+using WbsAlgorithms.Geometry;
+
+namespace WbsAlgorithmsTest.Geometry
+{
+    [TestFixture]
+    public class ClosestPairOfPointsTest
+    {
+        [TestCaseSource(nameof(ComputeClosestPairOfPointsTestCases))]
+        public void ComputeClosestPairOfPointsTest(Point[] points, Point[] expectedClosestPair, double expectedDistance)
+        {
+            const double Tolerance = 0.0000000001;
+
+            var result = ClosestPairOfPoints.ComputeBruteForce(points);
+
+            CollectionAssert.AreEqual(expectedClosestPair, result.ClosestPair);
+            Assert.AreEqual(expectedDistance, result.Distance, Tolerance);
+        }
+
+        private static IEnumerable<TestCaseData> ComputeClosestPairOfPointsTestCases()
+        {
+            yield return new TestCaseData(CreatePoints(1, 1, 4, 2), CreatePoints(1, 1, 4, 2), 3.1622776602).SetName("TwoPoints");
+            yield return new TestCaseData(CreatePoints(3, 5, 1, 4, -1, 2), CreatePoints(3, 5, 1, 4), 2.2360679775).SetName("ThreePoints");
+            yield return new TestCaseData(CreatePoints(-3, 3, 1, 1, 1.5, 5, 2, -3, 3, 2, 7, 1.5), CreatePoints(1, 1, 3, 2), 2.2360679775).SetName("SixPoints");
+            yield return new TestCaseData(CreatePoints(1, 5, 4, 4, 2, 3.5, 2.5, 7, 2, 2, 3, 3, 4, 6, 7, 5, 6, 3, 9, 5, 7, 7, 8, 2), CreatePoints(2, 3.5, 3, 3), 1.1180339887).SetName("LeftHalfPair");
+            yield return new TestCaseData(CreatePoints(1, 5, 4, 4, 2.5, 7, 2, 2, 3, 3, 4, 6, 7, 5, 6, 3, 9, 5, 7, 7, 8, 2, 7, 1.5), CreatePoints(8, 2, 7, 1.5), 1.1180339887).SetName("RightHalfPair");
+            yield return new TestCaseData(CreatePoints(1, 5, 4, 4, 2.5, 7, 2, 2, 3, 3, 4.5, 6, 7, 5, 6, 3, 9, 5, 7, 7, 8, 2, 5.5, 5.5), CreatePoints(4.5, 6, 5.5, 5.5), 1.1180339887).SetName("SplitPair");
+        }
+
+        #region Private helpers
+        private static Point[] CreatePoints(params double[] coordinates)
+        {
+            Debug.Assert(coordinates.Length % 2 == 0);
+
+            var pointCount = coordinates.Length / 2;
+            var points = new Point[pointCount];
+
+            for(var i = 0; i < pointCount; ++i)
+            {
+                var p = new Point(coordinates[2 * i], coordinates[2 * i + 1]);
+                points[i] = p;
+            }
+
+            return points;
+        }
+        #endregion
+    }
+}
