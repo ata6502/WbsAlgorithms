7b6fbf6ed83c688be6810cc63a4a2efac254b869
Leszek Taratuta <ata6502@hotmail.com>
2021-09-08 21:28:17 -0400

    Add circular rotation of strings

---
+++ WbsAlgorithms/Strings/SimpleAlgorithms.cs
@@ -0,0 +1,22 @@
+﻿namespace WbsAlgorithms.Strings
+{
+    public class SimpleAlgorithms
+    {
+        /// <summary>
+        /// Checks whether two strings are circular rotations of one another.
+        /// 
+        /// A string s1 is a circular rotation of a string s2 if it matches
+        /// when the characters are circularly shifted by any number of positions.
+        /// 
+        /// Example: ACTGACG is a circular shift of TGACGAC and vice versa.
+        /// 
+        /// [Sedgewick] 1.2.6 p.114 - Write a program that checks whether two 
+        /// strings are circular shifts of one another.
+        /// </summary>
+        /// <param name="s1">The first string</param>
+        /// <param name="s2">The second string</param>
+        /// <returns>True if the two strings are circular rotations of one another. False, otherwise.</returns>
+        public static bool AreStringsCircularRotations(string s1, string s2) =>
+            s1.Length == s2.Length && (s1 + s1).IndexOf(s2) != -1;
+    }
+}
---
+++ WbsAlgorithmsTest/Strings/SimpleAlgorithmsTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.Strings;
+
+namespace WbsAlgorithmsTest.Strings
+{
+    [TestFixture]
+    public class SimpleAlgorithmsTest
+    {
+        [TestCase("a", "a", true, TestName = "OneChar")]
+        [TestCase("ab", "ab", true, TestName = "TwoChars1")]
+        [TestCase("ab", "ba", true, TestName = "TwoChars2")]
+        [TestCase("ab", "aa", false, TestName = "TwoChars3")]
+        [TestCase("abc", "abc", true, TestName = "ThreeChars1")]
+        [TestCase("abc", "bca", true, TestName = "ThreeChars2")]
+        [TestCase("abc", "cab", true, TestName = "ThreeChars3")]
+        [TestCase("abc", "cac", false, TestName = "ThreeChars4")]
+        [TestCase("aabbcc", "abbcca", true, TestName = "SixChars1")]
+        [TestCase("abbcca", "bbccaa", true, TestName = "SixChars2")]
+        [TestCase("bbccaa", "ccaabb", true, TestName = "SixChars3")]
+        [TestCase("ACTGACG", "TGACGAC", true, TestName = "GenomicSequence1")]
+        [TestCase("GACCTGACTCC", "ACTCCGACCTG", true, TestName = "GenomicSequence2")]
+        [TestCase("GTCCGGTACT", "CTGTCCGGTA", true, TestName = "GenomicSequence3")]
+        [TestCase("CTTAGCTTA", "AGCTTACTT", true, TestName = "GenomicSequence4")]
+        [TestCase("TAGACTA", "ACTATAG", true, TestName = "GenomicSequence5")]
+        public void AreStringsCircularRotationsTest(string s1, string s2, bool expectedResult)
+        {
+            var actualResult = SimpleAlgorithms.AreStringsCircularRotations(s1, s2);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
