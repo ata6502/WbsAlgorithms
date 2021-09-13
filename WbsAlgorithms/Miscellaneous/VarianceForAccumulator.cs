119cf7d8577aef4dfe6438ba0c321beeb2f5e167
Leszek Taratuta <ata6502@hotmail.com>
2021-09-12 21:57:23 -0400

    Add variance for accumulator

---
+++ WbsAlgorithms/Miscellaneous/VarianceForAccumulator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WbsAlgorithms.Miscellaneous
+{
+    /// <summary>
+    /// [Sedgewick] 1.2.18 p.118 - Validate that the properties Variance and StdDev
+    /// compute the mean and variance of the numbers provided to the method AddValue.
+    /// </summary>
+    public class VarianceForAccumulator
+    {
+        private double _mean;
+        private double _sum;
+        private int _count;
+
+        /// <summary>
+        /// Adds a value to the accumulator. This implementation is less susceptible
+        /// to roundoff error than the implementation based on saving the sum of
+        /// the squares of the numbers.
+        /// </summary>
+        /// <param name="val">A value to add to the accumulator</param>
+        public void AddValue(double val)
+        {
+            ++_count;
+            _sum += 1.0 * (_count - 1) / _count * (val - _mean) * (val - _mean);
+            _mean += (val - _mean) / _count;
+        }
+
+        /// <summary>
+        /// Returns the mean of the numbers provided in the AddValue method.
+        /// </summary>
+        public double Mean => _mean;
+
+        /// <summary>
+        /// Returns the variance of the numbers provided in the AddValue method.
+        /// </summary>
+        public double Variance => _count > 1 ? _sum / (_count - 1) : 0;
+
+        /// <summary>
+        /// Returns the standard deviation of the numbers provided in the AddValue method.
+        /// </summary>
+        public double StdDev => Math.Sqrt(Variance);
+    }
+}
---
+++ WbsAlgorithmsTest/Miscellaneous/VarianceForAccumulatorTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.Miscellaneous;
+
+namespace WbsAlgorithmsTest.Miscellaneous
+{
+    [TestFixture]
+    public class VarianceForAccumulatorTest
+    {
+        // Values of the expected mean, variance, and standard deviation calculated using MatLab.
+        [TestCase(new double[] { 8 }, 8, 0, 0)]
+        [TestCase(new double[] { 5, 3 }, 4, 2, 1.414213562373095)]
+        [TestCase(new double[] { 3, 7, 11, 2 }, 5.75, 16.916666666666668, 4.112987559751022)]
+        [TestCase(new double[] { 7, 7, 7, 7, 7 }, 7, 0, 0)]
+        [TestCase(new double[] { 7, 7, 7, 7, 7, 1 }, 6, 6, 2.449489742783178)]
+        [TestCase(new double[] { 3, 8.2, 9, 3.2, 7.1, 0.3, 3.2 }, 4.857142857142857, 10.512857142857142, 3.242353642472878)]
+        [TestCase(new double[] { 2.0, 4.0, 5.0 }, 3.6666666666666665, 2.333333333333333, 1.5275252316519465)]
+        public void MeanVarianceStdDevTest(double[] values, double expectedMean, double expectedVariance, double expectedStdDev)
+        {
+            const double Tolerance = 0.000000000000001;
+
+            var acc = new VarianceForAccumulator();
+            foreach (var v in values)
+                acc.AddValue(v);
+
+            Assert.AreEqual(expectedMean, acc.Mean, Tolerance);
+            Assert.AreEqual(expectedVariance, acc.Variance, Tolerance);
+            Assert.AreEqual(expectedStdDev, acc.StdDev, Tolerance);
+        }
+    }
+}
