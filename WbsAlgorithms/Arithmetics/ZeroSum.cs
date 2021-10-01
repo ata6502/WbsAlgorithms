538db164bb683cda28ff136b5e9e0e7301f2f262
Leszek Taratuta <ata6502@hotmail.com>
2021-10-01 17:53:32 -0400

    Add linear ZeroSum algorithm

---
+++ WbsAlgorithms/Arithmetics/ZeroSum.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace WbsAlgorithms.Arithmetics
+{
+    public class ZeroSum
+    {
+        /// <summary>
+        /// Counts pairs that sum to zero. Complexity O(n^2)
+        /// </summary>
+        /// <param name="a">An array of integers</param>
+        /// <returns>The number of pairs that sum to zero</returns>
+        public static int CountPairsQuadratic(int[] a)
+        {
+            var len = a.Length;
+            var cnt = 0;
+
+            for (var i = 0; i < len; ++i)
+                for (var j = i + 1; j < len; ++j)
+                    if (a[i] + a[j] == 0)
+                        ++cnt;
+            return cnt;
+        }
+
+        /// <summary>
+        /// Counts pairs that sum to zero. Complexity O(n)
+        /// The input arrays needs to be sorted.
+        /// 
+        /// [Sedgewick] 1.4.15 p.210 - Develop an implementation of TwoSum that
+        /// uses a linear algorithm after the array is sorted.
+        /// </summary>
+        /// <param name="a">A sorted array of integers</param>
+        /// <returns>The number of pairs that sum to zero</returns>
+        public static int CountPairsLinear(int[] a)
+        {
+            var cnt = 0;            // the number of pairs that sum to zero
+            var i = 0;              // lower index
+            var j = a.Length - 1;   // upper index
+
+            while (i < j)
+            {
+                // A special case - a sequence of zeroes.
+                if (a[i] == 0 || a[j] == 0)
+                {
+                    // Find the boundary of the sequence of zeroes [i,j]
+                    while (a[i] != 0)
+                        ++i;
+                    while (a[j] != 0)
+                        --j;
+
+                    // Check if there are at least two zeroes in the array: j-i+1 > 1 => j-i > 0
+                    if (j - i > 0)
+                    {
+                        // There are j - i + 1 zeros in the array.
+                        var n = j - i + 1;
+
+                        // Calculate the number of pairs of zeroes.
+                        cnt += n * (n - 1) / 2;
+                    }
+
+                    // Stop the while loop.
+                    j = 0;
+                    i = 1;
+                }
+                else if (Math.Abs(a[i]) == a[j])
+                {
+                    // The lower number has to be negative.
+                    if (a[i] < 0)
+                    {
+                        var num = a[j];
+                        var neg = 0;
+                        var pos = 0;
+
+                        // Count the negative numbers.
+                        while (Math.Abs(a[i]) == num && a[i] < 0)
+                        {
+                            ++i;
+                            ++neg;
+                        }
+
+                        // Count the corresponding positive numbers.
+                        while (a[j] == num)
+                        {
+                            --j;
+                            ++pos;
+                        }
+
+                        // Calculate the number of pairs that sum to zero.
+                        cnt += neg * pos;
+                    }
+                    else
+                    {
+                        // If the lower number is positive it means that there is just
+                        // a sequence of equal positive numbers between a[i] and a[j].
+                        // We can stop the while loop.
+                        j = 0;
+                        i = 1;
+                    }
+                }
+                else if (Math.Abs(a[i]) > a[j])
+                    ++i;
+                else if (Math.Abs(a[i]) < a[j])
+                    --j;
+            }
+
+            return cnt;
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Arithmetics/ZeroSumTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.Arithmetics;
+
+namespace WbsAlgorithmsTest.Arithmetics
+{
+    [TestFixture]
+    public class ZeroSumTest
+    {
+        [TestCase(new[] { 1 }, 0)]
+        [TestCase(new[] { 1, 1 }, 0)]
+        [TestCase(new[] { 1, 1, 1 }, 0)]
+        [TestCase(new[] { -1 }, 0)]
+        [TestCase(new[] { -1, -1 }, 0)]
+        [TestCase(new[] { -1, -1, -1 }, 0)]
+        [TestCase(new[] { -1, -1, -1, -1 }, 0)]
+        [TestCase(new[] { -1, 1 }, 1)]
+        [TestCase(new[] { -1, -1, -1, 1, 1 }, 6)]
+        [TestCase(new[] { -2, 1, 2 }, 1)]
+        [TestCase(new[] { -2, 1, 2, 2 }, 2)]
+        [TestCase(new[] { -2, -2, 1, 2 }, 2)]
+        [TestCase(new[] { -2, -2, 0, 0, 2 }, 3)]
+        [TestCase(new[] { -3, -2, -2, -2, -1, 0, 1, 1, 1, 2, 4 }, 6)]
+        [TestCase(new[] { -8, -8, -5, 0, 5, 8, 8, 9, 10 }, 5)]
+        [TestCase(new[] { -3, -1, 1, 3, 5, 77 }, 2)]
+        [TestCase(new[] { -4, -2, 0, 0, 0, 0, 0, 0, 2 }, 16)]
+        [TestCase(new[] { -9, -9, -6, -3, 1, 1, 1, 3, 4, 7 }, 1)]
+        [TestCase(new[] { 0 }, 0, TestName = "CountPairs_1Zero")]
+        [TestCase(new[] { -7, -6, 0, 0 }, 1, TestName = "CountPairs_2Zeros1")]
+        [TestCase(new[] { 0, 0, 2, 3 }, 1, TestName = "CountPairs_2Zeros2")]
+        [TestCase(new[] { -7, -6, 0, 0, 2, 3 }, 1, TestName = "CountPairs_2Zeros3")]
+        [TestCase(new[] { 0, 0, 0, 1, 5, 7 }, 3, TestName = "CountPairs_3Zeros")]
+        [TestCase(new[] { -5, -1, 0, 0, 0, 0 }, 6, TestName = "CountPairs_4Zeros")]
+        [TestCase(new[] { 0, 0, 0, 0, 0 }, 10, TestName = "CountPairs_5Zeros")]
+        [TestCase(new[] { -4, -2, 0, 0, 0, 0, 0, 0, 3 }, 15, TestName = "CountPairs_6Zeros")]
+        public void CountPairsTest(int[] inputArray, int expectedCount)
+        {
+            var actualCount1 = ZeroSum.CountPairsQuadratic(inputArray);
+            Assert.AreEqual(expectedCount, actualCount1);
+
+            var actualCount2 = ZeroSum.CountPairsLinear(inputArray); // the inputArray has to be sorted
+            Assert.AreEqual(expectedCount, actualCount2);
+        }
+    }
+}
