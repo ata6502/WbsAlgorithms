d8207c03cba03588481c4608d2699321bc61a770
Leszek Taratuta <ata6502@hotmail.com>
2021-10-04 15:34:09 -0400

    Add a quadratic algorithm for 3-sum problem

--- WbsAlgorithms/Arithmetics/ZeroSum.cs
+++ WbsAlgorithms/Arithmetics/ZeroSum.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace WbsAlgorithms.Arithmetics
 {
@@ -23,7 +24,7 @@ namespace WbsAlgorithms.Arithmetics
 
         /// <summary>
         /// Counts pairs that sum to zero. Complexity O(n)
-        /// The input arrays needs to be sorted.
+        /// The input array needs to be sorted.
         /// 
         /// [Sedgewick] 1.4.15 p.210 - Develop an implementation of TwoSum that
         /// uses a linear algorithm after the array is sorted.
@@ -101,6 +102,123 @@ namespace WbsAlgorithms.Arithmetics
                 else if (Math.Abs(a[i]) < a[j])
                     --j;
             }
+
+            return cnt;
+        }
+
+        /// <summary>
+        /// Counts triples that sum to zero. Complexity O(n^3)
+        /// </summary>
+        /// <param name="a">An array of integers</param>
+        /// <returns>The number of triplets that sum to 0</returns>
+        public static int CountTripletsCubic(int[] a)
+        {
+            var len = a.Length;
+            var cnt = 0;
+
+            for (var i = 0; i < len; ++i)
+                for (var j = i + 1; j < len; ++j)
+                    for (var k = j + 1; k < len; ++k)
+                        if (a[i] + a[j] + a[k] == 0)
+                            ++cnt;
+            return cnt;
+        }
+
+        /// <summary>
+        /// Counts triples that sum to zero. Complexity O(n^2)
+        /// The input array needs to be sorted.
+        /// 
+        /// [Sedgewick] 1.4.15 p.210 - Develop a quadratic algorithm
+        /// for the 3-sum problem.
+        /// </summary>
+        /// <param name="a">A sorted array of integers</param>
+        /// <returns>The number of pairs that sum to zero</returns>
+        public static int CountTripletsQuadratic(int[] a)
+        {
+            var cnt = 0; // the number of triplets that sum to zero
+
+            for (var k = 0; k < a.Length; ++k)
+            {
+                var i = k + 1;         // lower index
+                var j = a.Length - 1;  // upper index
+
+                while (i < j)
+                {
+                    // Check if i and j indices point to a sequence of zeroes.
+                    if (a[i] == 0 && a[j] == 0)
+                    {
+                        // If so, stop the while loop.
+                        j = 0;
+                        i = 1;
+                    }
+                    else if (a[i] + a[j] + a[k] == 0)
+                    {
+                        // Because a[k] has a fixed value, we can count the number
+                        // of pairs {a[i],a[j]} that sum to -a[k]. Together,
+                        // the values a[k], a[i], and a[j] form a triplet.
+
+                        // A special case - the lower index i and the upper index j
+                        // point to the array elements that have the same value.
+                        if (a[i] == a[j])
+                        {
+                            // Calculate the number of elements.
+                            var n = j - i + 1;
+
+                            // Calculate the number of pairs that can be created
+                            // from n elements: cnt = n!/(2!*(n-2)!) = n*(n-1)/2
+                            cnt += n * (n - 1) / 2;
+
+                            // There are no more numbers to process. We can stop
+                            // the while loop.
+                            j = 0;
+                            i = 1;
+                        }
+                        else
+                        {
+                            // Keep both indices.
+                            var start_i = i;
+                            var start_j = j;
+
+                            // Wind up the index i as long as the triplet is zero.
+                            while (a[i] + a[start_j] + a[k] == 0 && i < start_j)
+                                ++i;
+
+                            // Wind down the index j as long as the triplet is zero.
+                            while (a[start_i] + a[j] + a[k] == 0 && j > start_i)
+                                --j;
+
+                            // Calculate the number of pairs that could be formed from
+                            // two intervals: [start_i, i] and [start_j, j]
+                            cnt += (i - start_i) * (start_j - j);
+                        }
+                    }
+                    else if (a[i] + a[j] + a[k] < 0)
+                        ++i;
+                    else
+                        --j;
+                }
+            }
+
+            cnt += CountZeroTriplets(a);
+
+            return cnt;
+        }
+
+        private static int CountZeroTriplets(int[] a)
+        {
+            // Scan the input array to count zeroes.
+            var n = 0; // the number of zeroes
+            var i = 0;
+            while (i < a.Length && a[i] <= 0)
+            {
+                if (a[i] == 0)
+                    ++n;
+                ++i;
+            }
+
+            // Calculate the number of triplets that can be formed from
+            // n zeroes: cnt = n! / (3!*(n-3)!)
+            var cnt = (n - 2) * (n - 1) * n / 6;
 
             return cnt;
         }
--- WbsAlgorithmsTest/Arithmetics/ZeroSumTest.cs
+++ WbsAlgorithmsTest/Arithmetics/ZeroSumTest.cs
@@ -40,5 +40,36 @@ namespace WbsAlgorithmsTest.Arithmetics
             var actualCount2 = ZeroSum.CountPairsLinear(inputArray); // the inputArray has to be sorted
             Assert.AreEqual(expectedCount, actualCount2);
         }
+
+        [TestCase(new[] { 1 }, 0)]
+        [TestCase(new[] { 1, 1 }, 0)]
+        [TestCase(new[] { 1, 1, 1 }, 0)]
+        [TestCase(new[] { -1, -1, -1, 1, 1 }, 0)]
+        [TestCase(new[] { -1 }, 0)]
+        [TestCase(new[] { -1, -1 }, 0)]
+        [TestCase(new[] { -1, -1, -1 }, 0)]
+        [TestCase(new[] { -1, -1, 0, 0, 0, 1, 2, 2 }, 9)]
+        [TestCase(new[] { -1, -1, 1, 1, 1, 1 }, 0)]
+        [TestCase(new[] { -2, -1, -1, 0, 0, 1, 1, 2, 2, 3 }, 17)]
+        [TestCase(new[] { -3, -1, -1, 1, 1, 1, 1, 2, 3, 3 }, 5)]
+        [TestCase(new[] { -4, -2, -2, 0, 0, 0, 1, 1, 1, 1, 2, 2 }, 26)]
+        [TestCase(new[] { -10, -10, -5, 0, 5, 10, 10, 15, 20 }, 8)]
+        [TestCase(new[] { -3, -2, 2, 3, 5, 88 }, 1)]
+        [TestCase(new[] { -10, -10, -10, 10 }, 0)]
+        [TestCase(new[] { 0, 0, 0, 0, 0, 0, 0 }, 35)]
+        [TestCase(new[] { -2, -1, 0, 0, 0, 0, 0, 0, 3 }, 21)]
+        [TestCase(new[] { -5, -5, -3, -2, -1, -1, -1, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 4, 5, 7 }, 100)]
+        [TestCase(new[] { -3, -2, -2, 0, 0, 0, 0, 0, 0, 0 }, 35)]
+        [TestCase(new[] { -14, -10, 5, 5, 7, 7 }, 2)]
+        [TestCase(new[] { -1, -1, -1, -1, -1, -1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 3, 3, 3 }, 160)]
+        [TestCase(new[] { -21, -20, -4, -2, -2, -2, 0, 1, 3, 5, 5, 6, 6, 6, 7, 7, 8, 8, 9, 9, 10, 12, 15, 19 }, 20)]
+        public void CountTripletsTest(int[] inputArray, int expectedCount)
+        {
+            var actualCount1 = ZeroSum.CountTripletsCubic(inputArray);
+            Assert.AreEqual(expectedCount, actualCount1);
+
+            var actualCount2 = ZeroSum.CountTripletsQuadratic(inputArray); // the inputArray has to be sorted
+            Assert.AreEqual(expectedCount, actualCount2);
+        }
     }
 }
