8afd59dfdb7b3fda17a8eb2f7f06bc99267957cb
Leszek Taratuta <ata6502@hotmail.com>
2021-10-14 21:31:36 -0400

    Add the closest pair in 1D alg and test

---
+++ WbsAlgorithms/PairMinMax/ClosestPair1D.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using WbsAlgorithms.Common;
+
+namespace WbsAlgorithms.PairMinMax
+{
+    public class ClosestPair1D
+    {
+        /// <summary>
+        /// Finds the closest pair using brute force. Complexity O(n^2)
+        /// </summary>
+        /// <param name="a">An array of double values. The array should contain at least two elements.</param>
+        /// <returns>The closest pair as a 2D point</returns>
+        public static Point FindPairBruteForce(double[] a)
+        {
+            Debug.Assert(a.Length >= 2);
+
+            var minDistance = double.MaxValue;
+            var minX = int.MaxValue;
+            var minY = int.MaxValue;
+
+            for (var x = 0; x < a.Length - 1; ++x)
+            {
+                for (var y = x + 1; y < a.Length; ++y)
+                {
+                    var distance = Math.Abs(a[y] - a[x]);
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        minX = x;
+                        minY = y;
+                    }
+                }
+            }
+
+            return new Point(a[minX], a[minY]);
+        }
+
+        /// <summary>
+        /// Finds the closest pair in a sorted array. Complexity O(n*log(n))
+        /// 
+        /// [Sedgewick] 1.4.16 p.210 - Find a closest pair in an array of double values.
+        /// The closest pair includes two values whose difference is no greater than 
+        /// the difference of any other pair. The running time of the program should be 
+        /// linearithmic in the worst case.
+        /// </summary>
+        /// <param name="a">An array of double values. The array should contain at least two elements.</param>
+        /// <returns>The closest pair as a 2D point. The input array is sorted.</returns>
+        public static Point FindPairLinearithmic(double[] a)
+        {
+            Debug.Assert(a.Length >= 2);
+
+            // Sort the input array.
+            Array.Sort(a);
+
+            var minDistance = double.MaxValue;
+            var minX = int.MaxValue;
+            var minY = int.MaxValue;
+
+            for (var i = 0; i < a.Length - 1; ++i)
+            {
+                var distance = Math.Abs(a[i + 1] - a[i]);
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    minX = i;
+                    minY = i + 1;
+                }
+            }
+
+            return new Point(a[minX], a[minY]);
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/PairMinMax/ClosestPair1DTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.Common;
+using WbsAlgorithms.PairMinMax;
+
+namespace WbsAlgorithmsTest.PairMinMax
+{
+    [TestFixture]
+    public class ClosestPair1DTest
+    {
+        [TestCase(new double[] { 1, 1 }, 1, 1)]
+        [TestCase(new double[] { 1, 5, 7 }, 5, 7)]
+        [TestCase(new double[] { -5.2, 9.4, 20, -10, 21.1, 40, 50, -20 }, 20, 21.1)]
+        [TestCase(new double[] { -4, -3, 0, 10, 20 }, -4, -3)]
+        [TestCase(new double[] { -10, -3, 0, 2, 4, 20 }, 0, 2)]
+        public void FindPairTest(double[] inputArray, double expectedX, double expectedY)
+        {
+            var pairExpected = new Point(expectedX, expectedY);
+
+            var pairBruteForce = ClosestPair1D.FindPairBruteForce(inputArray);
+            Assert.AreEqual(pairExpected, pairBruteForce);
+
+            var pairLinearithmic = ClosestPair1D.FindPairLinearithmic(inputArray);
+            Assert.AreEqual(pairExpected, pairLinearithmic);
+        }
+    }
+}
