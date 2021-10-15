42d39161ab9466c858480fdf33e2020c69e61659
Leszek Taratuta <ata6502@hotmail.com>
2021-10-14 21:16:23 -0400

    Implement IEquatable in the Point struct

--- WbsAlgorithms/Common/Point.cs
+++ WbsAlgorithms/Common/Point.cs
@@ -1,6 +1,8 @@
-﻿namespace WbsAlgorithms.Common
+﻿using System;
+
+namespace WbsAlgorithms.Common
 {
-    public struct Point
+    public struct Point : IEquatable<Point>
     {
         public double X { get; }
         public double Y { get; }
@@ -12,8 +14,25 @@
         }
 
         public override string ToString()
+            => $"({X:N1},{Y:N1})";
+
+        public override bool Equals(object other)
         {
-            return $"({X:N1},{Y:N1})";
+            if (!(other is Point)) 
+                return false;
+            return Equals((Point)other);
         }
+
+        public bool Equals(Point other)
+            => X == other.X && Y == other.Y;
+
+        public override int GetHashCode()
+            => HashCode.Combine(X, Y);
+
+        public static bool operator ==(Point p1, Point p2) 
+            => p1.Equals(p2);
+
+        public static bool operator !=(Point p1, Point p2) 
+            => !p1.Equals(p2);
     }
 }
