a41c232a655f1ab26c22fae4ff355a4ee6861025
Leszek Taratuta <ata6502@hotmail.com>
2021-10-18 20:02:58 -0400

    Add FarthestPair in one dimension

---
+++ WbsAlgorithms/PairMinMax/FarthestPair1D.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using WbsAlgorithms.Common;
+
+namespace WbsAlgorithms.PairMinMax
+{
+    public class FarthestPair1D
+    {
+        /// <summary>
+        /// Finds the farthest pair in an  unsorted array. Complexity O(n)
+        /// 
+        /// [Sedgewick] 1.4.17 p.210 - Find a farthest pair in an array of double values.
+        /// The farthest pair includes two values whose difference is no smaller than 
+        /// the difference of any other pair. The running time of the program should be 
+        /// linear in the worst case.
+        /// </summary>
+        /// <param name="a">An array of double values. The array should contain at least two elements.</param>
+        /// <returns>The farthest pair as a 2D point.</returns>
+        public static Point FindPair(double[] a)
+        {
+            Debug.Assert(a.Length >= 2);
+
+            var minIndex = 0;
+            var maxIndex = 0;
+
+            // Traverse all values in the array and find minimum and maximum.
+            // The difference between the minimum and maximum is the farthest pair.
+            for (int i = 0; i < a.Length; ++i)
+            {
+                if (a[i] < a[minIndex])
+                    minIndex = i;
+                else if (a[i] > a[maxIndex])
+                    maxIndex = i;
+            }
+
+            return new Point(a[minIndex], a[maxIndex]);
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/PairMinMax/FarthestPair1DTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.Common;
+using WbsAlgorithms.PairMinMax;
+
+namespace WbsAlgorithmsTest.PairMinMax
+{
+    [TestFixture]
+    public class FarthestPair1DTest
+    {
+        [TestCase(new double[] { 1, 1 }, 1, 1)]
+        [TestCase(new double[] { 1, 5, 7 }, 1, 7)]
+        [TestCase(new double[] { -20, 4, 1, 5, -67.8, 5.1, 7, 7.2 }, -67.8, 7.2)]
+        [TestCase(new double[] { -12, -11, -5, 0.1, 3.4, 5, 8 }, -12, 8)]
+        [TestCase(new double[] { 1, 60, 50, -15, -31, 20, 70, 100 }, -31, 100)]
+        [TestCase(new double[] { 3, 8, 9, -10, 8, -11, 3 }, -11, 9)]
+        [TestCase(new double[] { -30, 50, 2, 3 }, -30, 50)]
+        [TestCase(new double[] { 2, 1, 9, 7 }, 1, 9)]
+        [TestCase(new double[] { -5.2, 9.4, 20, -10, 21.1, 40, 50, -20 }, -20, 50)]
+        [TestCase(new double[] { -4, -3, 0, 10, 20 }, -4, 20)]
+        [TestCase(new double[] { -10, -3, 0, 2, 4, 20 }, -10, 20)]
+        public void FindPairTest(double[] inputArray, double expectedX, double expectedY)
+        {
+            var pairExpected = new Point(expectedX, expectedY);
+            var pairActual = FarthestPair1D.FindPair(inputArray);
+            Assert.AreEqual(pairExpected, pairActual);
+        }
+    }
+}
