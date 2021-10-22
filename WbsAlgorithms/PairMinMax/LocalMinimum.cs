9b5424a13eeb3cd07ec85fb7e2dc308e9893d47d
Leszek Taratuta <ata6502@hotmail.com>
2021-10-22 17:07:51 -0400

    Add local minimum in array

---
+++ WbsAlgorithms/PairMinMax/LocalMinimum.cs
@@ -0,0 +1,89 @@
+﻿namespace WbsAlgorithms.PairMinMax
+{
+    public class LocalMinimum
+    {
+        private static int _comparisonCounter;
+
+        /// <summary>
+        /// Returns an index of a local minimum in an array. The local minimum a[i]
+        /// is such that a[i-1] < a[i] < a[i+1]. The method finds just one local minimum
+        /// even if multiple minimums exist in the array.
+        /// 
+        /// [Sedgewick] 1.4.18 p.210 - Find a local minimum in an array. Your program
+        /// should use ~2lg(N) comparisons in the worst case (N is the size of the input
+        /// array).
+        /// </summary>
+        /// <param name="a">An array containing distinct integer values</param>
+        /// <returns>An index of a local minimum and the number of comparisons</returns>
+        public static (int Index, int Counter) FindLocalMinimumIndexInArray(int[] a)
+        {
+            _comparisonCounter = 0;
+
+            // Special cases:
+            // - one element in the input array
+            // - two elements in the input array
+            if (a.Length == 1)
+                return (0, _comparisonCounter);
+            if (a.Length == 2)
+                return (a[0] < a[1] ? 0 : 1, _comparisonCounter);
+
+            var index = FindLocalMinimumIndexInArrayRecursive(a, 0, a.Length);
+            return (index, _comparisonCounter);
+        }
+
+        private static int FindLocalMinimumIndexInArrayRecursive(int[] a, int low, int high)
+        {
+            int mid = (low + high) / 2;
+
+            // Special cases:
+            // - the local minimum is the first or the second element 
+            // - the local minimum is the last or the one-before-last element
+            if (mid == 0)
+                return a[mid] < a[mid + 1] ? mid : mid + 1;
+            if (mid == a.Length - 1)
+                return a[mid] < a[mid - 1] ? mid : mid - 1;
+
+            // The following approach is simple and provides correct results.
+            // However, it requires more than 2*ln(n) comparisons.
+            /*
+            _comparisonCounter += 2;
+
+            // Examine the middle value and its two neighbours. 
+            if (a[mid] < a[mid - 1] && a[mid] < a[mid + 1])
+                // a[mid] is a local minimum.
+                return mid;
+
+            _comparisonCounter += 1;
+
+            // Search in the half with the smaller neighbour as it may be the local minimum.
+            if (a[mid - 1] < a[mid + 1])
+                return FindLocalMinimumIndexInArrayRecursive(a, low, mid - 1);
+            else
+                return FindLocalMinimumIndexInArrayRecursive(a, mid + 1, high);
+            */
+
+            // The following approach is more complicated but it requires less than 
+            // 2*ln(n) comparisons in almost all cases.
+            _comparisonCounter++;
+
+            if (a[mid] < a[mid - 1])
+            {
+                _comparisonCounter++;
+
+                if (a[mid] < a[mid + 1])
+                    return mid;
+                return FindLocalMinimumIndexInArrayRecursive(a, mid + 1, high);
+            }
+            else
+            {
+                _comparisonCounter++;
+
+                if (a[mid - 1] < a[mid + 1])
+                    return FindLocalMinimumIndexInArrayRecursive(a, low, mid - 1);
+                else
+                    return FindLocalMinimumIndexInArrayRecursive(a, mid + 1, high);
+            }
+
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/PairMinMax/LocalMinimumTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using WbsAlgorithms.PairMinMax;
+
+namespace WbsAlgorithmsTest.PairMinMax
+{
+    [TestFixture]
+    public class LocalMinimumTest
+    {
+        [TestCase(new int[] { 1 }, 0)] // a[0]=1
+        [TestCase(new int[] { 1, 2 }, 0)] // a[0]=1
+        [TestCase(new int[] { 2, 1 }, 1)] // a[1]=1
+        [TestCase(new int[] { 1, 2, 3 }, 0)] // a[0]=1
+        [TestCase(new int[] { 2, 1, 3 }, 1)] // a[1]=1
+        [TestCase(new int[] { 3, 2, 1 }, 2)] // a[2]=1
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 0)] // a[0]=1
+        [TestCase(new int[] { 7, 6, 5, 4, 3, 2, 1 }, 6)] // a[6]=1
+        [TestCase(new int[] { 8, 7, 6, 5, 4, 3, 2, 1 }, 7)] // a[7]=1
+        [TestCase(new int[] { 1, 0, 2, 3, 4, 5, 6, 7 }, 1)] // a[1]=0
+        [TestCase(new int[] { 2, 1, 0, 3, 4, 5, 6, 7 }, 2)] // a[2]=0
+        [TestCase(new int[] { 3, 2, 1, 0, 4, 5, 6, 7 }, 3)] // a[3]=0 - This is the only case that requires more comparisons than 2*lg(n): #of compares: 8 ~ 2*lg(n)=6
+        [TestCase(new int[] { 4, 3, 2, 1, 0, 5, 6, 7 }, 4)] // a[4]=0
+        [TestCase(new int[] { 5, 4, 3, 2, 1, 0, 6, 7 }, 5)] // a[5]=0
+        [TestCase(new int[] { 6, 5, 4, 3, 2, 1, 0, 7 }, 6)] // a[6]=0
+        [TestCase(new int[] { 1, 0, 4, 8, 5, 2, -1, 7 }, 6)] // a[6]=-1
+        [TestCase(new int[] { 8, 9, -1, 3, 4, 1, -3, 2, -2, -4, 6 }, 9)] // a[0]=8 or a[2]=-1 or a[6]=-3 or a[9]=-4
+        [TestCase(new int[] { 10, -9, 20, 25, 21, 40, 50, -20 }, 4)] // a[1]=-9 or a[4]=21 or a[7]=-20
+        [TestCase(new int[] { -4, -3, 9, 4, 10, 2, 20 }, 3)] // a[0]=-4 or a[3]=4 or a[5]=2
+        [TestCase(new int[] { 5, -3, -5, -6, -7, -8 }, 5)] // a[5]=-8
+        public void LocalMinimumInArrayTest(int[] inputArray, int expectedIndex)
+        {
+            // The method finds one local minimum. It's always the same minimum.
+            var result = LocalMinimum.FindLocalMinimumIndexInArray(inputArray);
+
+            // In some cases the number of comparisons is greater than 2*lg(n), n = inputArray.Length
+            Console.WriteLine($"#of compares: {result.Counter} ~ 2*lg(n)={2*Math.Log(inputArray.Length, 2.0):F0}");
+
+            Assert.AreEqual(expectedIndex, result.Index);
+        }
+    }
+}
