d99cbb6d2573cf0dbd6a5755169f594351ec8657
Leszek Taratuta <ata6502@hotmail.com>
2021-11-12 17:30:46 -0500

    Add StringArithmeticHelper

---
+++ WbsAlgorithms/Arithmetics/StringArithmeticHelper.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Text;
+
+namespace WbsAlgorithms.Arithmetics
+{
+    /// <summary>
+    /// The StringArithmetic class provides static methods that perform arithmetic
+    /// operations on integers represented as strings. It allows us to do calculations 
+    /// on numbers greater than C#'s maximum and minimum integers.
+    /// </summary>
+    public class StringArithmeticHelper
+    {
+        /// <summary>
+        /// Makes the length of the input integers to be a power of 2. Also, the resulting
+        /// lengths of both numbers are equal. The method achieves that by prepending leading 0s 
+        /// to the beginning of the numbers. This operation at most doubles the lengths of 
+        /// the numbers.
+        /// </summary>
+        /// <param name="x">The first integer in the string representation</param>
+        /// <param name="y">The second integer in the string representation</param>
+        /// <returns>The same integers as input but with leading 0s if needed</returns>
+        public static (string x, string y) PadLeftZeros(string x, string y)
+        {
+            // Find the closest power of two for the number with the greater number of digits.
+            int pow = (int)Math.Ceiling(Math.Log(Math.Max(x.Length, y.Length), 2.0));
+
+            // Calculate the required number of digits.
+            int digitCount = (2 << (pow - 1));
+
+            // Pad the numbers with 0s and return.
+            return (
+                x.PadLeft(digitCount, '0'),
+                y.PadLeft(digitCount, '0'));
+        }
+
+        /// <summary>
+        /// Adds two integers represented by strings.
+        /// </summary>
+        /// <param name="x">The first integer in the string representation</param>
+        /// <param name="y">The second integer in the string representation</param>
+        /// <returns>The sum of the input integers as a string.</returns>
+        public static string AddNumbers(string x, string y)
+        {
+            var sb = new StringBuilder();
+            var carry = 0;
+
+            for (int i = x.Length - 1, j = y.Length - 1; i >= 0 || j >= 0; --i, --j)
+            {
+                // Check if the i and j indices are less than 0 because the numbers
+                // may have different lengths.
+
+                // Convert the first number to a numerical value.
+                var xnum = 0;
+                if (i >= 0)
+                    xnum = int.Parse(x[i].ToString());
+
+                // Convert the second number to a numerical value.
+                var ynum = 0;
+                if (j >= 0)
+                    ynum = int.Parse(y[j].ToString());
+
+                // Add both numbers taking into account the value of carry.
+                var n = xnum + ynum + carry;
+
+                // Determine the carry value.
+                carry = n / 10;
+
+                // Keep only the remainder value as long as there are still more digits to process.
+                // Otherwise, if both i=0 and j=0, we have achieved the leading digit. We just need
+                // to keep this digit.
+                if (i > 0 || j > 0)
+                    n = n % 10;
+
+                sb.Insert(0, n.ToString());
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Subtracts two numbers represented as strings. The method has a constraint that
+        /// the first number has to be greater or equal to the second number, i.e., x >= y
+        /// </summary>
+        /// <param name="x">The first integer in the string representation. It has to be greater then the second integer.</param>
+        /// <param name="y">The second integer in the string representation</param>
+        /// <returns>The result of the subtraction x - y. Because x >= y, the result is always greater than 0.</returns>
+        public static string SubtractNumbers(string x, string y)
+        {
+            var sb = new StringBuilder();
+            var borrow = 0;
+
+            for (int i = x.Length - 1, j = y.Length - 1; i >= 0 || j >= 0; --i, --j)
+            {
+                var xnum = 0;
+                if (i >= 0)
+                    xnum = int.Parse(x[i].ToString());
+
+                var ynum = 0;
+                if (j >= 0)
+                    ynum = int.Parse(y[j].ToString());
+
+                if (xnum - borrow < ynum)
+                {
+                    if (xnum == 0)
+                        xnum = 10 - borrow;
+                    else
+                        xnum = xnum - borrow + 10;
+
+                    borrow = 1;
+                }
+                else
+                {
+                    xnum = xnum - borrow;
+                    borrow = 0;
+                }
+
+                var n = xnum - ynum;
+
+                sb.Insert(0, n.ToString());
+            }
+
+            // Remove prefix 0s if any.
+            var result = sb.ToString().TrimStart('0');
+            if (result.Length == 0)
+                result = "0";
+
+            return result;
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Arithmetics/StringArithmeticHelperTest.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WbsAlgorithms.Arithmetics;
+
+namespace WbsAlgorithmsTest.Arithmetics
+{
+    [TestFixture]
+    public class StringArithmeticHelperTest
+    {
+        [TestCase("0", "0", "0", "0")]
+        [TestCase("0", "1", "0", "1")]
+        [TestCase("1", "0", "1", "0")]
+        [TestCase("0", "10", "00", "10")]
+        [TestCase("1", "10", "01", "10")]
+        [TestCase("10", "1", "10", "01")]
+        [TestCase("123", "1", "0123", "0001")]
+        [TestCase("123", "321", "0123", "0321")]
+        [TestCase("12345", "22", "00012345", "00000022")]
+        public void MakeLengthEqualAndPowerOfTwoTest(string x, string y, string paddedX, string paddedY)
+        {
+            var result = StringArithmeticHelper.PadLeftZeros(x, y);
+
+            Assert.AreEqual(paddedX, result.x);
+            Assert.AreEqual(paddedY, result.y);
+        }
+
+        [TestCase("1", "2", "3")]
+        [TestCase("8", "3", "11")]
+        [TestCase("2", "12", "14")]
+        [TestCase("13", "5", "18")]
+        [TestCase("90", "12", "102")]
+        [TestCase("99", "2", "101")]
+        [TestCase("100", "450", "550")]
+        [TestCase("998", "5", "1003")]
+        public void AddNumbersTest(string x, string y, string expectedResult)
+        {
+            var result = StringArithmeticHelper.AddNumbers(x, y);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase("2", "1", "1")]
+        [TestCase("8", "3", "5")]
+        [TestCase("12", "2", "10")]
+        [TestCase("13", "5", "8")]
+        [TestCase("90", "12", "78")]
+        [TestCase("99", "2", "97")]
+        [TestCase("450", "100", "350")]
+        [TestCase("998", "5", "993")]
+        public void SubtractNumbersTest(string x, string y, string expectedResult)
+        {
+            var result = StringArithmeticHelper.SubtractNumbers(x, y);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
