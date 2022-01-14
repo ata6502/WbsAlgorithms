a1644221f03f0b106462d637ee08f3674ce50805
Leszek Taratuta <ata6502@hotmail.com>
2022-01-13 20:32:18 -0500

    Add throwing eggs problem

---
+++ WbsAlgorithms/Searching/ThrowingEggsProblem.cs
@@ -0,0 +1,30 @@
+﻿namespace WbsAlgorithms.Searching
+{
+    public class ThrowingEggsProblem
+    {
+        public static int GetHighestFloor(bool[] floors)
+        {
+            return GetHighestFloorRecursive(floors, 0, floors.Length - 1);
+        }
+
+        private static int GetHighestFloorRecursive(bool[] floors, int low, int high)
+        {
+            if (low > high)
+            {
+                if (high == -1)
+                    return high;
+                else if (low == floors.Length)
+                    return low;
+                else
+                    return high + 1;
+            }
+
+            var mid = (low + high) / 2;
+
+            if (floors[mid])
+                return GetHighestFloorRecursive(floors, mid + 1, high);
+            else
+                return GetHighestFloorRecursive(floors, low, mid - 1);
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Searching/ThrowingEggsProblemTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.Searching;
+
+namespace WbsAlgorithmsTest.Searching
+{
+    [TestFixture]
+    public class ThrowingEggsProblemTest
+    {
+        [TestCase("x", -1, TestName = "1Floor_None")]
+        [TestCase(".", 1, TestName = "1Floor")]
+        [TestCase("xx", -1, TestName = "2Floors_None")]
+        [TestCase(".x", 1, TestName = "2Floors_1")]
+        [TestCase("..", 2, TestName = "2Floors_2")]
+        [TestCase("xxx", -1, TestName = "3Floors_None")]
+        [TestCase(".xx", 1, TestName = "3Floors_1")]
+        [TestCase("..x", 2, TestName = "3Floors_2")]
+        [TestCase("...", 3, TestName = "3Floors_3")]
+        [TestCase("xxxxxxxxxxxx", -1, TestName = "12Floors_None")]
+        [TestCase(".xxxxxxxxxxx", 1, TestName = "12Floors_1")]
+        [TestCase("..xxxxxxxxxx", 2, TestName = "12Floors_2")]
+        [TestCase("...xxxxxxxxx", 3, TestName = "12Floors_3")]
+        [TestCase("....xxxxxxxx", 4, TestName = "12Floors_4")]
+        [TestCase(".....xxxxxxx", 5, TestName = "12Floors_5")]
+        [TestCase("......xxxxxx", 6, TestName = "12Floors_6")]
+        [TestCase(".......xxxxx", 7, TestName = "12Floors_7")]
+        [TestCase("........xxxx", 8, TestName = "12Floors_8")]
+        [TestCase(".........xxx", 9, TestName = "12Floors_9")]
+        [TestCase("..........xx", 10, TestName = "12Floors_10")]
+        [TestCase("...........x", 11, TestName = "12Floors_11")]
+        [TestCase("............", 12, TestName = "12Floors_12")]
+        public void GetHighestFloorTest(string floors, int expectedFloor)
+        {
+            var actualFloor = ThrowingEggsProblem.GetHighestFloor(ConvertToBoolArray(floors));
+            Assert.AreEqual(expectedFloor, actualFloor);
+        }
+
+        private bool[] ConvertToBoolArray(string s)
+        {
+            var a = new bool[s.Length];
+            for (var i = 0; i < s.Length; ++i)
+                a[i] = s[i] == '.';
+            return a;
+        }
+    }
+}
