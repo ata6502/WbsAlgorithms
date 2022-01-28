9c75f3e5dfd0f3fe1bd1db12d50c109a0a3a316e
Leszek Taratuta <ata6502@hotmail.com>
2022-01-27 20:58:46 -0500

    Add tests for collinear points

--- WbsAlgorithmsTest/PairPointMinMax/CollinearPointsTest.cs
+++ WbsAlgorithmsTest/PairPointMinMax/CollinearPointsTest.cs
@@ -10,23 +10,30 @@ namespace WbsAlgorithmsTest.PairPointMinMax
     public class CollinearPointsTest
     {
         [TestCaseSource(nameof(TestCases))]
-        public void CountTripletsBruteForceTest(Point[] points, int expectedTripletCount)
+        public void CountTripletsBruteForceTest(Point[] points, int expectedTripletCount, bool useForCubicYTest)
         {
-            var actualTripletCount = CollinearPoints.CountTriplesBruteForce(points);
-            Assert.AreEqual(expectedTripletCount, actualTripletCount);
-        }
+            var actualTripletCountBruteForce = CollinearPoints.CountTriplesBruteForce(points);
+            Assert.AreEqual(expectedTripletCount, actualTripletCountBruteForce);
 
-        [TestCaseSource(nameof(TestCases))]
-        public void CountTriplesUsingSlopesTest(Point[] points, int expectedTripletCount)
-        {
-            var actualTripletCount = CollinearPoints.CountTriplesUsingSlopes(points);
-            Assert.AreEqual(expectedTripletCount, actualTripletCount);
+            var actualTripletCountUsingSlopes = CollinearPoints.CountTriplesUsingSlopes(points);
+            Assert.AreEqual(expectedTripletCount, actualTripletCountUsingSlopes);
+
+            if (useForCubicYTest)
+            {
+                var actualTripletCountWithCubicY = CollinearPoints.CountTriplesWithCubicY(points);
+                Assert.AreEqual(expectedTripletCount, actualTripletCountWithCubicY);
+            }
         }
 
         private static IEnumerable<TestCaseData> TestCases()
         {
-            yield return new TestCaseData(CreatePoints(2, 8, 3, 5, 7, 2, 5, 9, 1, 8), 0).SetName("NoTriples");
-            yield return new TestCaseData(CreatePoints(2, 3, 3, 5, 7, 2, 5, 9, 1, 8), 1).SetName("1Triple");
+            yield return new TestCaseData(CreatePoints(2, 8, 3, 5, 7, 2, 5, 9, 1, 8), 0, true).SetName("NoTriplets");
+            yield return new TestCaseData(CreatePoints(2, 3, 3, 5, 7, 2, 5, 9, 1, 8), 1, false).SetName("1Triplet_1");
+            yield return new TestCaseData(CreatePoints(-3, 4, 3, 2, 6, 1), 1, false).SetName("1Triplet_2");
+            yield return new TestCaseData(CreatePoints(1, 1, 2, 8, -3, -27), 1, true).SetName("1Triplet_3");
+            yield return new TestCaseData(CreatePoints(-5, -125, -2, -8, 7, 343, 3, 27, -1, -1), 2, true).SetName("2Triplets_2");
+            yield return new TestCaseData(CreatePoints(-3, 4, 3, 2, 6, 1, 9, 0), 4, false).SetName("4Triplets_1");
+            yield return new TestCaseData(CreatePoints(0, 1.5, 1, 2.5, 2, 3.5, 3, 4.5, 4, 5.5, 5, 6.5), 20, false).SetName("20Triplets");
         }
     }
 }
