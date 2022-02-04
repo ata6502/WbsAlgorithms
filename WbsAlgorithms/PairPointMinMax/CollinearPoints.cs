35f78a2a5f40ef753d8f4adf034027e8d5583be8
Leszek Taratuta <ata6502@hotmail.com>
2022-02-03 20:49:10 -0500

    Add O(n^2) algorithm for 3-collinearily

--- WbsAlgorithms/PairPointMinMax/CollinearPoints.cs
+++ WbsAlgorithms/PairPointMinMax/CollinearPoints.cs
@@ -13,12 +13,12 @@ namespace WbsAlgorithms.PairPointMinMax
     /// Three points A,B,C are collinear if the lines AB and BC have the same slopes.
     /// Having A=(x1,y1), B=(x2,y2), C=(x3,y3) we need to check if 
     /// (y2-y1) / (x2-x1) = (y3-y2) / (x3-x2)  -or-  (y2-y1) / (x2-x1) = (y3-y1) / (x3-x1)
+    /// We have a special case when both slopes are zero.
     ///
     /// References: 
     /// https://stackoverflow.com/questions/2734301/given-a-set-of-points-find-if-any-of-the-three-points-are-collinear
     /// https://stackoverflow.com/questions/3813681/checking-to-see-if-3-points-are-on-the-same-line
     /// https://stackoverflow.com/questions/4179581/what-is-the-most-efficient-algorithm-to-find-a-straight-line-that-goes-through-m
-    /// https://www.algebra.com/algebra/homework/Length-and-distance/Length-and-distance.faq.question.530663.html
     public class CollinearPoints
     {
         /// <summary>
@@ -38,15 +38,18 @@ namespace WbsAlgorithms.PairPointMinMax
                     var A = points[i];
                     var B = points[j];
                     var denominator1 = B.X - A.X;
-                    var m1 = Math.Abs(denominator1) <= double.Epsilon ? 0.0 : (B.Y - A.Y) / denominator1;
+                    var m1 = Math.Abs(denominator1) <= double.Epsilon ? double.PositiveInfinity : (B.Y - A.Y) / denominator1;
 
                     for (var k = j + 1; k < len; ++k)
                     {
                         var C = points[k];
-                        var denominator2 = C.X - B.X;
-                        var m2 = Math.Abs(denominator2) <= double.Epsilon ? 0.0 : (C.Y - B.Y) / denominator2;
+                        var denominator2 = C.X - A.X;
+                        var m2 = Math.Abs(denominator2) <= double.Epsilon ? double.PositiveInfinity : (C.Y - A.Y) / denominator2;
 
-                        if (Math.Abs(m2 - m1) < double.Epsilon)
+                        var bothSlopesInfinity = m1 == double.PositiveInfinity && m2 == double.PositiveInfinity;
+                        var sameXs = A.X == B.X && B.X == C.X;
+
+                        if ((bothSlopesInfinity && sameXs) || (!bothSlopesInfinity && Math.Abs(m2 - m1) < double.Epsilon))
                             ++tripletCount;
                     }
                 }
@@ -56,8 +59,9 @@ namespace WbsAlgorithms.PairPointMinMax
         }
 
         /// <summary>
-        /// Determines the number of triples that fall on the same line by comparing slopes
-        /// between the lines given by pairs of points.
+        /// Determines the number of triples that fall on the same line by calculating and storing slopes 
+        /// between all pairs of points first. Then, iterating over those slopes and checking if the points 
+        /// with the same slope are collinear.
         /// </summary>
         /// <param name="points">A set of points in the plane</param>
         /// <returns>The number of triples that fall on the same line</returns>
@@ -76,7 +80,7 @@ namespace WbsAlgorithms.PairPointMinMax
                     var A = points[i];
                     var B = points[j];
                     var denominator = B.X - A.X;
-                    var slope = Math.Abs(denominator) <= double.Epsilon ? 0.0 : (B.Y - A.Y) / denominator;
+                    var slope = Math.Abs(denominator) <= double.Epsilon ? double.PositiveInfinity : (B.Y - A.Y) / denominator;
 
                     if (slopes.TryGetValue(slope, out var indices))
                     {
@@ -93,25 +97,46 @@ namespace WbsAlgorithms.PairPointMinMax
                 }
             }
 
+            // Iterate over slopes and check if the points with the same slope are collinear.
             var tripletCount = 0;
-            for (var i = 0; i < len; ++i)
+            foreach(var slopeIndices in slopes)
             {
-                for (var j = i + 1; j < len; ++j)
-                {
-                    var A = points[i];
-                    var B = points[j];
-                    var denominator = B.X - A.X;
-                    var slope = Math.Abs(denominator) <= double.Epsilon ? 0.0 : (B.Y - A.Y) / denominator;
+                var indices = slopeIndices.Value;
+                var slope = slopeIndices.Key;
 
-                    if (slopes.TryGetValue(slope, out var indices))
+                // Initialize the starting point A. The pointCount n will contain the number
+                // of points that are collinear with the point A. We need to initialize n to 1
+                // to include the point A.
+                var A = points[indices[0]];
+                var n = 1;
+
+                for (var i = 1; i < indices.Count; ++i)
+                {
+                    var B = points[indices[i]];
+                    var denominator = B.X - A.X;
+                    var nextSlope = Math.Abs(denominator) <= double.Epsilon ? double.PositiveInfinity : (B.Y - A.Y) / denominator;
+
+                    var bothSlopesInfinity = slope == double.PositiveInfinity && nextSlope == double.PositiveInfinity;
+                    var sameXs = A.X == B.X;
+
+                    if ((bothSlopesInfinity && sameXs) || (!bothSlopesInfinity && Math.Abs(nextSlope - slope) < double.Epsilon))
+                        ++n;
+                    else
                     {
-                        foreach(var index in indices)
-                        {
-                            if (index > i && index > j)
-                                ++tripletCount;
-                        }
+                        // Calculate the number of triplets using combinatorics: the number of ways to choose a k-elements
+                        // from an n-elements is given by the formula for binomial coefficients: C(n, k) = n! / ((n - k)! k!)
+                        // For k = 3 we have:
+                        tripletCount += (n - 2) * (n - 1) * n / 6;
+
+                        // We have another group of points with the same slope but not collinear with the previous points.
+                        // Initialize a new starting point and pointCount.
+                        A = points[indices[i]];
+                        n = 1;
                     }
                 }
+
+                // Calculate the number of triplets for the last group of points.
+                tripletCount += (n - 2) * (n - 1) * n / 6;
             }
 
             return tripletCount;
