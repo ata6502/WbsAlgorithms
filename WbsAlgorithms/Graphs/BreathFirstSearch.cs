4cc6e6413f9da90761b9033af70c8625effb6270
Leszek Taratuta <ata6502@hotmail.com>
2022-02-11 17:29:22 -0500

    Add Breath-First Search algorithm

---
+++ WbsAlgorithms/Graphs/BreathFirstSearch.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using WbsAlgorithms.Common;
+
+namespace WbsAlgorithms.Graphs
+{
+    public class BreathFirstSearch
+    {
+        /// <summary>
+        /// Perfroms Breath-First Search on the given graph. Uses an iterative approach.
+        /// 
+        /// [AlgoIlluminated-2] p.27 Breath-First Search
+        /// </summary>
+        /// <param name="g">A graph in adjacency-list represenation</param>
+        /// <param name="sourceVertex">One of the graph's vertices</param>
+        /// <returns>A list of explored vertices</returns>
+        public static List<int> ExploreIteratively(Graph g, int sourceVertex)
+        {
+            Debug.Assert(g.VertexCount > 0);
+
+            var q = new Queue<int>();
+            q.Enqueue(sourceVertex);
+
+            // An collection of explored vertices.
+            var e = new HashSet<int>(g.VertexCount);
+            e.Add(sourceVertex);
+
+            while (q.Count > 0)
+            {
+                var v = q.Dequeue();
+
+                // Traverse each edge in the v's adjacency list.
+                foreach (var w in g[v])
+                {
+                    // Check if the vertex w is unexplored.
+                    if (!e.TryGetValue(w, out var explored))
+                    {
+                        // Mark the vertex w as explored.
+                        e.Add(w);
+
+                        // Add the vertex w to the end of the queue.
+                        q.Enqueue(w);
+                    }
+                }
+            }
+
+            return ConvertAndSort(e);
+        }
+
+        // Convert a HashSet containing explored vertices to a sorted list.
+        private static List<int> ConvertAndSort(HashSet<int> e)
+        {
+            var list = e.ToList();
+            list.Sort();
+            return list;
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Graphs/BreathFirstSearchTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using WbsAlgorithms.Graphs;
+using WbsAlgorithmsTest.Utilities;
+
+namespace WbsAlgorithmsTest.Graphs
+{
+    [TestFixture]
+    public class BreathFirstSearchTest
+    {
+        [Test]
+        public void SingleComponentTest()
+        {
+            // GraphDAG2 contains three components.
+            var g = DataReader.ReadGraph(@"Data\GraphDAG2.txt");
+
+            // The vertex 1 is the source vertex. We can reach the entire graph from this vertex.
+            var expected = new List<int> { 1, 2, 3, 4, 5, 6 };
+            CollectionAssert.AreEqual(expected, BreathFirstSearch.ExploreIteratively(g, 1));
+
+            // The vertex 6 is the sink. We are not able to reach any other vertices.
+            expected = new List<int> { 6 };
+            CollectionAssert.AreEqual(expected, BreathFirstSearch.ExploreIteratively(g, 6));
+
+            // From the vertex 4 we can reach vertices 5 and 6.
+            expected = new List<int> { 4, 5, 6 };
+            CollectionAssert.AreEqual(expected, BreathFirstSearch.ExploreIteratively(g, 4));
+        }
+
+        [Test]
+        public void MultipleComponentsTest()
+        {
+            // Graph1 contains three components.
+            var g = DataReader.ReadGraph(@"Data\Graph1.txt");
+
+            // From vertex 1 we can reach vertices 4 and 7 (a single component).
+            // Vertex 1 (as well as 4 and 7) belongs to the sink component.
+            var expected = new List<int> { 1, 4, 7 };
+            CollectionAssert.AreEqual(expected, BreathFirstSearch.ExploreIteratively(g, 1));
+
+            // From vertex 2 we can reach all vertices (all three components).
+            // Vertex 2 (as well as 5 and 8) belongs to the source component.
+            expected = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            CollectionAssert.AreEqual(expected, BreathFirstSearch.ExploreIteratively(g, 2));
+
+            // From vertex 3 we can reach vertices 6, 9, 7, 1, 4 (two components).
+            expected = new List<int> { 1, 3, 4, 6, 7, 9 };
+            CollectionAssert.AreEqual(expected, BreathFirstSearch.ExploreIteratively(g, 3));
+        }
+    }
+}
