2d0026008f10c026e9b729a7e4d6932509885f4f
Leszek Taratuta <ata6502@hotmail.com>
2022-02-16 20:33:22 -0500

    Add more tests to the shortest paths algorithm

--- WbsAlgorithmsTest/Graphs/ShortestPathTest.cs
+++ WbsAlgorithmsTest/Graphs/ShortestPathTest.cs
@@ -18,6 +18,22 @@ namespace WbsAlgorithmsTest.Graphs
 
         private static IEnumerable<TestCaseData> TestCases()
         {
+            yield return new TestCaseData(@"Data\Graph1.txt", 1,
+                new Dictionary<int, int> { { 1, 0 }, { 4, 1 }, { 7, 2 }, { 9, -1 }, { 3, -1 }, { 6, -1 }, { 8, -1 }, { 2, -1 }, { 5, -1 } }).SetName("Graph1_start1");
+            yield return new TestCaseData(@"Data\Graph1.txt", 3,
+                new Dictionary<int, int> { { 1, 4 }, { 4, 5 }, { 7, 3 }, { 9, 2 }, { 3, 0 }, { 6, 1 }, { 8, -1 }, { 2, -1 }, { 5, -1 } }).SetName("Graph1_start3");
+            yield return new TestCaseData(@"Data\Graph1.txt", 5,
+                new Dictionary<int, int> { { 1, 6 }, { 4, 7 }, { 7, 5 }, { 9, 4 }, { 3, 5 }, { 6, 3 }, { 8, 2 }, { 2, 1 }, { 5, 0 } }).SetName("Graph1_start5");
+            yield return new TestCaseData(@"Data\Graph1.txt", 1,
+                new Dictionary<int, int> { { 1, 0 }, { 4, 1 }, { 7, 2 }, { 9, -1 }, { 3, -1 }, { 6, -1 }, { 8, -1 }, { 2, -1 }, { 5, -1 } }).SetName("Graph1_start1");
+            yield return new TestCaseData(@"Data\Graph2.txt", 6,
+                new Dictionary<int, int> { { 1, -1 }, { 2, -1 }, { 3, -1 }, { 4, 2 }, { 5, 1 }, { 6, 0 }, { 7, 1 }, { 8, 2 } }).SetName("Graph2_start6");
+            yield return new TestCaseData(@"Data\Graph3.txt", 1,
+                new Dictionary<int, int> { { 1, 0 }, { 2, 1 }, { 3, 2 }, { 4, 3 }, { 5, -1 }, { 6, -1 }, { 7, -1 }, { 8, -1 } }).SetName("Graph3_start1");
+            yield return new TestCaseData(@"Data\Graph3.txt", 5,
+                new Dictionary<int, int> { { 1, -1 }, { 2, -1 }, { 3, -1 }, { 4, 1 }, { 5, 0 }, { 6, -1 }, { 7, -1 }, { 8, -1 } }).SetName("Graph3_start5");
+            yield return new TestCaseData(@"Data\Graph3.txt", 7,
+                new Dictionary<int, int> { { 1, -1 }, { 2, -1 }, { 3, -1 }, { 4, 3 }, { 5, -1 }, { 6, 2 }, { 7, 0 }, { 8, 1 } }).SetName("Graph3_start7");
             yield return new TestCaseData(@"Data\GraphDAG2.txt", 1, 
                 new Dictionary<int, int> { { 1, 0 }, { 2, 1 }, { 3, 1 }, { 4, 2 }, { 5, 3 }, { 6, 2 } }).SetName("GraphDAG2_start1");
             yield return new TestCaseData(@"Data\GraphDAG2.txt", 4,
