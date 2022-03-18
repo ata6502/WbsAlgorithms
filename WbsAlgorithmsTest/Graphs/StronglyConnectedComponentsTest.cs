069cfacf5699a1653a4dca06a6c8e9c7c54606a9
Leszek Taratuta <ata6502@hotmail.com>
2022-03-17 20:37:24 -0400

    Test leaders in SCC test

--- WbsAlgorithmsTest/Graphs/StronglyConnectedComponentsTest.cs
+++ WbsAlgorithmsTest/Graphs/StronglyConnectedComponentsTest.cs
@@ -10,7 +10,7 @@ namespace WbsAlgorithmsTest.Graphs
     public class StronglyConnectedComponentsTest
     {
         [TestCaseSource(nameof(TestCases))]
-        public void GetComponentsTest(string filename, string expectedTopSccSizes)
+        public void GetComponentsTest(string filename, string expectedTopSccSizesWithLeaders)
         {
             var graph = DataReader.ReadGraph(filename);
             var reversed = DataReader.ReverseGraph(graph);
@@ -22,27 +22,30 @@ namespace WbsAlgorithmsTest.Graphs
             var topFive = (from c in components.Skip(1)
                            group c by c into g
                            orderby g.Count() descending
-                           select g.Count()).Take(5).ToList();
+                           select (g.First(), g.Count())).Take(5).ToList();
 
             // If the graph has fewer than five components, set the remaining sizes to 0.
             for (var i = topFive.Count; i < 5; ++i)
-                topFive.Add(0);
-            var actualTopSccSizes = string.Join(',', topFive);
+                topFive.Add((0, 0));
+            var actualTopSccSizesWithLeaders = string.Join("", topFive).Replace(" ", "");
 
-            // Assert sizes of the top five strongly connected components. 
-            Assert.AreEqual(expectedTopSccSizes, actualTopSccSizes);
+            // Assert sizes and leaders of the top five strongly connected components. 
+            Assert.AreEqual(expectedTopSccSizesWithLeaders, actualTopSccSizesWithLeaders);
         }
 
         private static IEnumerable<TestCaseData> TestCases()
         {
-            yield return new TestCaseData(@"Data\Graph1.txt", "3,3,3,0,0").SetName("Graph1"); // a 9-vertex 11-edge graph
-            yield return new TestCaseData(@"Data\Graph2.txt", "3,3,2,0,0").SetName("Graph2"); // an 8-vertex 14-edge graph
-            yield return new TestCaseData(@"Data\Graph3.txt", "3,3,1,1,0").SetName("Graph3"); // an 8-vertex 9-edge graph
-            yield return new TestCaseData(@"Data\Graph4.txt", "7,1,0,0,0").SetName("Graph4"); // a 8-vertex 11-edge graph
-            yield return new TestCaseData(@"Data\Graph5.txt", "6,3,2,1,0").SetName("Graph5"); // a 12-vertex 20-edge graph
-            yield return new TestCaseData(@"Data\Graph6.txt", "4,3,3,1,0").SetName("Graph6"); // an 11-vertex 18-edge graph
-            yield return new TestCaseData(@"Data\Graph7.txt", "1,1,1,1,0").SetName("Graph7"); // a 4-vertex 3-edge graph
-            yield return new TestCaseData(@"Data\GraphBig.txt", "434821,968,459,313,211").SetName("LargeGraph"); // an 875714-vertex graph
+            // (leader, the size of SCC)
+
+            yield return new TestCaseData(@"Data\Graph1.txt", "(9,3)(3,3)(6,3)(0,0)(0,0)").SetName("Graph1"); // a 9-vertex 11-edge graph
+            yield return new TestCaseData(@"Data\Graph2.txt", "(3,3)(6,3)(8,2)(0,0)(0,0)").SetName("Graph2"); // an 8-vertex 14-edge graph
+            yield return new TestCaseData(@"Data\Graph3.txt", "(7,3)(3,3)(8,1)(4,1)(0,0)").SetName("Graph3"); // an 8-vertex 9-edge graph
+            yield return new TestCaseData(@"Data\Graph4.txt", "(8,7)(1,1)(0,0)(0,0)(0,0)").SetName("Graph4"); // a 8-vertex 11-edge graph
+            yield return new TestCaseData(@"Data\Graph5.txt", "(12,6)(5,3)(7,2)(2,1)(0,0)").SetName("Graph5"); // a 12-vertex 20-edge graph
+            yield return new TestCaseData(@"Data\Graph6.txt", "(8,4)(3,3)(11,3)(4,1)(0,0)").SetName("Graph6"); // an 11-vertex 18-edge graph
+            yield return new TestCaseData(@"Data\Graph7.txt", "(3,1)(4,1)(1,1)(2,1)(0,0)").SetName("Graph7"); // a 4-vertex 3-edge graph
+            yield return new TestCaseData(@"Data\Graph8.txt", "(4,2)(2,1)(1,1)(0,0)(0,0)").SetName("Graph8"); // a 4-vertex 4-edge graph
+            yield return new TestCaseData(@"Data\GraphBig.txt", "(615986,434821)(617403,968)(798411,459)(367067,313)(709991,211)").SetName("LargeGraph"); // an 875714-vertex graph
         }
     }
 }
