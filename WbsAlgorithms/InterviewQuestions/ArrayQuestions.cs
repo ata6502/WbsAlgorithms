922a7b1978273f297727ad809c42327de19386b3
Leszek Taratuta <ata6502@hotmail.com>
2022-03-24 17:25:36 -0400

    Add array interview questions with tests

--- WbsAlgorithms/InterviewQuestions/ArrayQuestions.cs
+++ WbsAlgorithms/InterviewQuestions/ArrayQuestions.cs
@@ -1,10 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace WbsAlgorithms.InterviewQuestions
 {
+    // Questions:
+    // 1. How to find duplicated numbers in an array? [FindDuplicatedNumbers]
     public class ArrayQuestions
     {
+        // Returns an array of numbers that occur in the input array at least twice.
+        public static int[] FindDuplicatedNumbersUsingDictionary(int[] a)
+        {
+            Debug.Assert(a != null);
+
+            var d = new Dictionary<int, int>();
+
+            foreach(var n in a)
+            {
+                if (d.TryGetValue(n, out _))
+                    ++d[n];
+                else
+                    d[n] = 1;
+            }
+
+            var duplicates = new List<int>();
+            foreach(var kvp in d)
+            {
+                if (kvp.Value > 1)
+                    duplicates.Add(kvp.Key);
+            }
+
+            return duplicates.ToArray();
+        }
+        
+        public static int[] FindDuplicatedNumbersUsingLinq(int[] a)
+        {
+            Debug.Assert(a != null);
+
+            var groups = from n in a
+                         group n by n into g
+                         where g.Count() > 1
+                         select g.Key;
+
+            return groups.ToArray();
+        }
     }
 }
---
+++ WbsAlgorithmsTest/InterviewQuestionsTest/ArrayQuestionsTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.InterviewQuestions;
+
+namespace WbsAlgorithmsTest.InterviewQuestionsTest
+{
+    [TestFixture]
+    public class ArrayQuestionsTest
+    {
+        [TestCase(new int[] { 1 }, new int[] { })]
+        [TestCase(new int[] { 1, 1, 2 }, new int[] { 1 })]
+        [TestCase(new int[] { 1, 2, 2 }, new int[] { 2 })]
+        [TestCase(new int[] { 3, 3, 1, 2, 2, 3 }, new int[] { 3, 2 })]
+        [TestCase(new int[] { 1, 3, 2, 1 }, new int[] { 1 })]
+        [TestCase(new int[] { 3, 2, 1, 3, 4, 2, 1 }, new int[] { 3, 2, 1 })]
+        public void FindDuplicatedNumbersTest(int[] inputArray, int[] expectedDuplicates)
+        {
+            CollectionAssert.AreEqual(expectedDuplicates, ArrayQuestions.FindDuplicatedNumbersUsingDictionary(inputArray));
+            CollectionAssert.AreEqual(expectedDuplicates, ArrayQuestions.FindDuplicatedNumbersUsingLinq(inputArray));
+        }
+    }
+}
