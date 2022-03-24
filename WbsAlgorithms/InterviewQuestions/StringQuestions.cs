226964219814aa5608d51f981a2c3822a8c6039a
Leszek Taratuta <ata6502@hotmail.com>
2022-03-23 21:44:45 -0400

    Add string interview questions

--- WbsAlgorithms/InterviewQuestions/StringQuestions.cs
+++ WbsAlgorithms/InterviewQuestions/StringQuestions.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 
 namespace WbsAlgorithms.InterviewQuestions
 {
     // Questions:
     // 1. How to reverse a string? [ReverseString]
+    // 2. How to determine if a string is a palindrome? [IsPalindrome]
+    // 3. How to find duplicated characters in a string? [FindDuplicatedCharacters]
     public class StringQuestions
     {
+        // Returns a string formed from characters that are duplicated in the input 
+        // string at least twice.
+        public static string FindDuplicatedCharactersUsingLinq(string s)
+        {
+            var a = s.ToCharArray();
+
+            var groups = from c in a
+                         group c by c into g
+                         where g.Count() > 1
+                         select g.Key;
+
+            return new string(groups.ToArray());
+        }
+
+        public static string FindDuplicatedCharactersUsingDictionary(string s)
+        {
+            var d = new Dictionary<char, int>();
+
+            foreach (var c in s)
+            {
+                if (d.TryGetValue(c, out _))
+                    ++d[c];
+                else
+                    d[c] = 1;
+            }
+
+            var sb = new StringBuilder();
+            foreach (var kvp in d)
+                if (kvp.Value > 1)
+                    sb.Append(kvp.Key);
+
+            return sb.ToString();
+        }
+
+        // Determines if the input string is a palindrome. A palindrome is
+        // a string that reads the same backward as forward.
+        public static bool IsPalindrome(string s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; ++i, --j)
+            {
+                if (s[i] != s[j])
+                    return false;
+            }
+
+            return true;
+        }
+
         public static string ReverseString(string s)
         {
             Debug.Assert(s != null);
--- WbsAlgorithmsTest/InterviewQuestionsTest/StringQuestionsTest.cs
+++ WbsAlgorithmsTest/InterviewQuestionsTest/StringQuestionsTest.cs
@@ -6,6 +6,40 @@ namespace WbsAlgorithmsTest.InterviewQuestionsTest
     [TestFixture]
     public class StringQuestionsTest
     {
+        [TestCase("a", "")]
+        [TestCase("abb", "b")]
+        [TestCase("abc", "")]
+        [TestCase("aacbb", "ab")]
+        [TestCase("abbcfccdeafb", "abcf")]
+        [TestCase("abcdefghte", "e")]
+        public void FindDuplicatedCharactersTest(string inputString, string expectedDuplicates)
+        {
+            Assert.AreEqual(expectedDuplicates, StringQuestions.FindDuplicatedCharactersUsingLinq(inputString));
+            Assert.AreEqual(expectedDuplicates, StringQuestions.FindDuplicatedCharactersUsingDictionary(inputString));
+        }
+
+        [TestCase("a", true)]
+        [TestCase("ab", false)]
+        [TestCase("aa", true)]
+        [TestCase("abb", false)]
+        [TestCase("aba", true)]
+        [TestCase("aaa", true)]
+        [TestCase("abca", false)]
+        [TestCase("abba", true)]
+        [TestCase("aaaa", true)]
+        [TestCase("abcad", false)]
+        [TestCase("bbcba", false)]
+        [TestCase("abbba", true)]
+        [TestCase("abcba", true)]
+        [TestCase("aaaaa", true)]
+        [TestCase("abcbaa", false)]
+        [TestCase("abccba", true)]
+        [TestCase("aaaaaa", true)]
+        public void IsPalindromeTest(string inputString, bool isPalindrome)
+        {
+            Assert.AreEqual(isPalindrome, StringQuestions.IsPalindrome(inputString));
+        }
+
         [TestCase("a", "a")]
         [TestCase("ab", "ba")]
         [TestCase("abc", "cba")]
