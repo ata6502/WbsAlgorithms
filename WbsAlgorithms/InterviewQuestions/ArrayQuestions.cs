3a6d79743909115954f167ca61f0508c39942825
Leszek Taratuta <ata6502@hotmail.com>
2022-03-25 17:06:59 -0400

    Add array interview questions

--- WbsAlgorithms/InterviewQuestions/ArrayQuestions.cs
+++ WbsAlgorithms/InterviewQuestions/ArrayQuestions.cs
@@ -6,8 +6,68 @@ namespace WbsAlgorithms.InterviewQuestions
 {
     // Questions:
     // 1. How to find duplicated numbers in an array? [FindDuplicatedNumbers]
+    // 2. How to remove duplicates from an array? [RemoveDuplicates]
+    // 3. How to find a missing integer in an array that contains a range of consecutive integers? [FindMissingValue]
     public class ArrayQuestions
     {
+        // Returns a missing value from an array containing a range of consecutive integers; -1 if no value is missing.
+        public static int FindMissingValue(int[] a)
+        {
+            Debug.Assert(a != null);
+
+            if (a.Length == 0)
+                return -1;
+
+            var n = a[0];
+            for(var i = 0; i < a.Length; ++i)
+            {
+                if (a[i] != n)
+                    return n;
+                ++n;
+            }
+
+            return -1;
+        }
+
+        public static int FindMissingValueUsingFormula(int[] a)
+        {
+            Debug.Assert(a != null);
+
+            if (a.Length == 0)
+                return -1;
+
+            var sum = 0;
+            foreach (var n in a)
+                sum += n;
+
+            var len = a.Length + 1;
+            var calculatedSum = len * (2 * a[0] + (len - 1)) / 2;
+
+            var missingValue = calculatedSum - sum;
+            if (missingValue == 0)
+                return -1;
+            return missingValue;
+        }
+
+        // Returns an array without duplicates i.e., only with distinct elements.
+        public static int[] RemoveDuplicates(int[] a)
+        {
+            Debug.Assert(a != null);
+
+            var distinctNumbers = new HashSet<int>();
+            foreach (var n in a)
+                if (!distinctNumbers.TryGetValue(n, out _))
+                    distinctNumbers.Add(n);
+            return distinctNumbers.ToArray();
+        }
+
+        public static int[] RemoveDuplicatesUsingLinq(int[] a)
+        {
+            Debug.Assert(a != null);
+
+            return a.Distinct().ToArray();
+        }
+
         // Returns an array of numbers that occur in the input array at least twice.
         public static int[] FindDuplicatedNumbersUsingDictionary(int[] a)
         {
--- WbsAlgorithmsTest/InterviewQuestionsTest/ArrayQuestionsTest.cs
+++ WbsAlgorithmsTest/InterviewQuestionsTest/ArrayQuestionsTest.cs
@@ -6,6 +6,28 @@ namespace WbsAlgorithmsTest.InterviewQuestionsTest
     [TestFixture]
     public class ArrayQuestionsTest
     {
+        [TestCase(new int[] { 1, 3 }, 2)]
+        [TestCase(new int[] { 2, 4 }, 3)]
+        [TestCase(new int[] { 1, 2, 3, 5 }, 4)]
+        [TestCase(new int[] { 3, 4, 6, 7, 8 }, 5)]
+        public void FindMissingValueTest(int[] inputArray, int expectedMissingValue)
+        {
+            Assert.AreEqual(expectedMissingValue, ArrayQuestions.FindMissingValue(inputArray));
+            Assert.AreEqual(expectedMissingValue, ArrayQuestions.FindMissingValueUsingFormula(inputArray));
+        }
+
+        [TestCase(new int[] { 1 }, new int[] { 1 })]
+        [TestCase(new int[] { 1, 1 }, new int[] { 1 })]
+        [TestCase(new int[] { 1, 2 }, new int[] { 1, 2 })]
+        [TestCase(new int[] { 1, 2, 1 }, new int[] { 1, 2 })]
+        [TestCase(new int[] { 1, 1, 1, 2, 2, 2, 1, 2, 1, 2 }, new int[] { 1, 2 })]
+        [TestCase(new int[] { 1, 1, 1, 8, 2, 2, 2, 3, 1, 1, 1, 3, 3 }, new int[] { 1, 8, 2, 3 })]
+        [TestCase(new int[] { 3, 2, 1, 3, 4, 2, 1 }, new int[] { 3, 2, 1, 4 })]
+        public void RemoveDuplicatesTest(int[] inputArray, int[] expectedDistinctValues)
+        {
+            CollectionAssert.AreEqual(expectedDistinctValues, ArrayQuestions.RemoveDuplicates(inputArray));
+        }
+
         [TestCase(new int[] { 1 }, new int[] { })]
         [TestCase(new int[] { 1, 1, 2 }, new int[] { 1 })]
         [TestCase(new int[] { 1, 2, 2 }, new int[] { 2 })]
