6c76c773a00b876287e777756e93765e31008786
Leszek Taratuta <ata6502@hotmail.com>
2022-07-06 21:59:33 -0400

    Add an additional test case for UndirectedGraph8

--- WbsAlgorithmsTest/Graphs/DepthFirstPathsTest.cs
+++ WbsAlgorithmsTest/Graphs/DepthFirstPathsTest.cs
@@ -41,6 +41,37 @@ namespace WbsAlgorithmsTest.Graphs
                     new PathFromSourceToDestination(4, new[] { 0, 5, 3, 4 }),
                     new PathFromSourceToDestination(5, new[] { 0, 5 })
                 }).SetName("Graph7");
+
+            yield return new TestCaseData(
+                @"Data\UndirectedGraph8.txt", 0, new[] { 0, 1, 2, 3, 4, 5, 6 },
+                new PathFromSourceToDestination[]
+                {
+                    new PathFromSourceToDestination(0, new[] { 0 }),
+                    new PathFromSourceToDestination(1, new[] { 0, 1 }),
+                    new PathFromSourceToDestination(2, new[] { 0, 2 }),
+                    new PathFromSourceToDestination(3, new[] { 0, 6, 4, 5, 3 }),
+                    new PathFromSourceToDestination(4, new[] { 0, 6, 4 }),
+                    new PathFromSourceToDestination(5, new[] { 0, 6, 4, 5 }),
+                    new PathFromSourceToDestination(6, new[] { 0, 6 })
+                }).SetName("Graph8_0");
+
+            yield return new TestCaseData(
+                @"Data\UndirectedGraph8.txt", 7, new[] { 7, 8 },
+                new PathFromSourceToDestination[]
+                {
+                    new PathFromSourceToDestination(7, new[] { 7 }),
+                    new PathFromSourceToDestination(8, new[] { 7, 8 })
+                }).SetName("Graph8_7");
+
+            yield return new TestCaseData(
+                @"Data\UndirectedGraph8.txt", 9, new[] { 9, 10, 11, 12 },
+                new PathFromSourceToDestination[]
+                {
+                    new PathFromSourceToDestination(9, new[] { 9 }),
+                    new PathFromSourceToDestination(10, new[] { 9, 10 }),
+                    new PathFromSourceToDestination(11, new[] { 9, 11 }),
+                    new PathFromSourceToDestination(12, new[] { 9, 11, 12 })
+                }).SetName("Graph8_9");
         }
 
         public class PathFromSourceToDestination
