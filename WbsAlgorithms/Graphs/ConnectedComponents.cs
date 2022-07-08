cc69216d48e423087b972ad251ffb8f47caafb9b
Leszek Taratuta <ata6502@hotmail.com>
2022-07-08 11:41:21 -0400

    Add connected components algorithm on undirected graphs

---
+++ WbsAlgorithms/Graphs/ConnectedComponents.cs
@@ -0,0 +1,78 @@
+﻿using WbsAlgorithms.Common;
+
+namespace WbsAlgorithms.Graphs
+{
+    /// <summary>
+    /// DFS to find connected components in an undirected graph.
+    /// [Sedgewick] p.544
+    /// </summary>
+    public class ConnectedComponents
+    {
+        private bool[] _explored;
+
+        // The vertex-to-componentId mapping. It associates the same id
+        // to every vertex in each component.
+        private int[] _id;
+
+        // The number of components.
+        private int _count;
+
+        /// <summary>
+        /// Uses DFS to find connected components in an undirected graph.
+        /// </summary>
+        /// <param name="g">An undirected graph</param>
+        public ConnectedComponents(Graph g)
+        {
+            _explored = new bool[g.VertexCount];
+            _id = new int[g.VertexCount];
+
+            for (var s = 0; s < g.VertexCount; ++s)
+            {
+                // Find a source vertex (an unexplored vertex) to perform
+                // for DFS in each component.
+                if (!_explored[s])
+                {
+                    Dfs(g, s);
+
+                    // Proceed to the next component.
+                    ++_count;
+                }
+            }
+        }
+
+        // Mark all vertices in a single component as explored.
+        // Also, keep the vertex-to-componentId mapping for all
+        // these vertices.
+        private void Dfs(Graph g, int v)
+        {
+            _explored[v] = true;
+            _id[v] = _count;
+
+            foreach (var w in g[v])
+            {
+                if (!_explored[w])
+                    Dfs(g, w);
+            }
+        }
+
+        /// <summary>
+        /// Determines if the input vertices are connected.
+        /// </summary>
+        /// <param name="v">The first vertex</param>
+        /// <param name="w">The second vertex</param>
+        /// <returns>True if the vertices are connected; false otherwise</returns>
+        public bool AreConnected(int v, int w) => _id[v] == _id[w];
+
+        /// <summary>
+        /// Returns a component id a given vertex belongs to.
+        /// </summary>
+        /// <param name="v">The input vertex</param>
+        /// <returns>Component id between 0 and ComponentCount-1</returns>
+        public int GetComponentId(int v) => _id[v];
+
+        /// <summary>
+        /// Returns the number of connected components.
+        /// </summary>
+        public int ComponentCount => _count;
+    }
+}
---
+++ WbsAlgorithmsTest/Graphs/ConnectedComponentsTest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using WbsAlgorithms.Graphs;
+using WbsAlgorithmsTest.Utilities;
+
+namespace WbsAlgorithmsTest.Graphs
+{
+    [TestFixture]
+    public class ConnectedComponentsTest
+    {
+        [TestCaseSource(nameof(TestCases))]
+        public void GetConnectedComponentsTest(string graphFile, int expectedComponentCount, int[][] expectedComponents)
+        {
+            var g = DataReader.ReadGraph(graphFile);
+            var alg = new ConnectedComponents(g);
+            var cnt = alg.ComponentCount;
+
+            // Verify the number of connected components.
+            Assert.AreEqual(expectedComponentCount, cnt);
+
+            // Create queues that will contain vertices of each component.
+            var components = new Queue<int>[cnt];
+            for (var i = 0; i < cnt; ++i)
+                components[i] = new Queue<int>();
+
+            // Populate queues with component vertices: each queue contains
+            // vertices for a single component.
+            for (var v = 0; v < g.VertexCount; ++v)
+                components[alg.GetComponentId(v)].Enqueue(v);
+
+            // Verify vertices in each component.
+            for (var i = 0; i < cnt; ++i)
+            {
+                var j = 0;
+                foreach (var v in components[i])
+                {
+                    Assert.AreEqual(expectedComponents[i][j], v);
+                    ++j;
+                }
+            }
+        }
+
+        private static IEnumerable<TestCaseData> TestCases()
+        {
+            // Component ids for each vertex: [0,0,0,0,0,0,1,1,2,2,2,2]
+            yield return new TestCaseData(
+                @"Data\UndirectedGraph8.txt", 3,
+                new int[][]
+                {
+                    new[]{ 0, 1, 2, 3, 4, 5, 6 },
+                    new[]{ 7, 8 },
+                    new[]{ 9, 10, 11, 12 }
+                }).SetName("Graph8");
+        }
+    }
+}
