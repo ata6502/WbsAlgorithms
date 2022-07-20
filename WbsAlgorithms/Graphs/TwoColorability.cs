de35bc7a6700a9c0e7583e05263a7a7996e805d4
Leszek Taratuta <ata6502@hotmail.com>
2022-07-19 21:03:32 -0400

    Add TwoColorability graph algorithm

---
+++ WbsAlgorithms/Graphs/TwoColorability.cs
@@ -0,0 +1,52 @@
+﻿using WbsAlgorithms.Common;
+
+namespace WbsAlgorithms.Graphs
+{
+    /// <summary>
+    /// Determines if the vertices of a graph can be assigned one of two colors
+    /// in such a way that no edge connects vertices of the same color.
+    /// [Sedgewick] p.547
+    /// </summary>
+    public class TwoColorability
+    {
+        private bool[] _explored;
+        private bool[] _color;
+
+        /// <summary>
+        /// Indicates if the graph is bipartite.
+        /// </summary>
+        public bool IsBipartite { get; private set; } = true;
+
+        /// <summary>
+        /// Detects if a graph is bipartite.
+        /// </summary>
+        /// <param name="g">The input graph</param>
+        public TwoColorability(Graph g)
+        {
+            _explored = new bool[g.VertexCount];
+            _color = new bool[g.VertexCount];
+
+            for (var s = 0; s < g.VertexCount; ++s)
+                if (!_explored[s])
+                    Dfs(g, s);
+        }
+
+        private void Dfs(Graph g, int v)
+        {
+            _explored[v] = true;
+
+            foreach (var w in g[v])
+            {
+                if (!_explored[w])
+                {
+                    _color[w] = !_color[v];
+                    Dfs(g, w);
+                }
+                else if (_color[w] == _color[v])
+                {
+                    IsBipartite = false;
+                }
+            }
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Graphs/TwoColorabilityTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using WbsAlgorithms.Graphs;
+using WbsAlgorithmsTest.Utilities;
+
+namespace WbsAlgorithmsTest.Graphs
+{
+    [TestFixture]
+    public class TwoColorabilityTest
+    {
+        [TestCaseSource(nameof(TestCases))]
+        public void IsBipartiteTest(string graphFile, bool isBipartite)
+        {
+            var g = DataReader.ReadGraph(graphFile);
+            var alg = new TwoColorability(g);
+
+            Assert.AreEqual(isBipartite, alg.IsBipartite);
+        }
+
+        private static IEnumerable<TestCaseData> TestCases()
+        {
+            yield return new TestCaseData(@"Data\UndirectedGraph6.txt", true).SetName("Graph6");
+            yield return new TestCaseData(@"Data\UndirectedGraph7.txt", false).SetName("Graph7");
+            yield return new TestCaseData(@"Data\UndirectedGraph8.txt", false).SetName("Graph8");
+            yield return new TestCaseData(@"Data\UndirectedGraph9.txt", true).SetName("Graph9");
+            yield return new TestCaseData(@"Data\UndirectedGraph10.txt", true).SetName("Graph10");
+        }
+    }
+}
