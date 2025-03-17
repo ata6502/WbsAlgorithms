32a2dc7c0d3e4331b91a11195abd91c503c8606a
ata6502 <ata6502@hotmail.com>
2025-03-17 17:06:37 -0400

    Add comments to FindMax value in a unimodal array

--- WbsAlgorithms/PairPointMinMax/UnimodalArray.cs
+++ WbsAlgorithms/PairPointMinMax/UnimodalArray.cs
@@ -2,6 +2,16 @@
 {
     public class UnimodalArray
     {
+        /*
+            Maximum value in a unimodal array.
+
+            [AlgoIlluminated-1] - Problem 3.2 (p.91, eb.103)
+            You are given a unimodal array of n distinct elements, meaning that its entries are in increasing order up 
+            until its maximum element, after which its elements are in decreasing order. Give an algorithm to compute 
+            the maximum element of a unimodal array that runs in O(log(n)) time.
+
+            In the notation of the master method, a = 1, b = 2, and d = 0, so the running time is O(log(n)) (by case #1).
+        */
         public static int FindMax(int[] a)
         {
             int left = 0, right = a.Length - 1;
@@ -22,6 +32,7 @@
                 }
             }
 
+            // The loop continues until left == right, which is the peak index.
             return a[left];
         }
 
