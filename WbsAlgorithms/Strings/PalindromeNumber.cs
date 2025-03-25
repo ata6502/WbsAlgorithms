956754b24193cfae63d06c142ad2959e186fba2b
ata6502 <ata6502@hotmail.com>
2025-03-25 08:59:44 -0400

    Add PalindromeNumber algorithm

---
+++ WbsAlgorithms/Strings/PalindromeNumber.cs
@@ -0,0 +1,81 @@
+﻿namespace WbsAlgorithms.Strings
+{
+    /*
+        Palindrome Number
+
+        [leetcode]: https://leetcode.com/problems/palindrome-number/
+
+        Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+        Example 1: 121 is a palindrome.
+        Example 2: -121 is not a palindrome
+        Example 3: 10 is not a palindrome
+
+        Coud you solve it without converting the integer to a string?
+    */
+
+    // Time complexity: O(log10(n)). We divide the input by 10 for every iteration.
+    // Space complexity: O(1)
+    public class PalindromeNumber
+    {
+        // Solution from the website: https://leetcode.com/problems/palindrome-number/solution/
+        public static bool IsPalindrome1(int n)
+        {
+            // Special cases.
+            // - When n < 0, n is not a palindrome.
+            // - If the last digit of the number is 0, in order to be a palindrome, the first 
+            //   digit of the number also needs to be 0. Only 0 satisfy this property.
+            if (n < 0 || (n != 0 && n % 10 == 0))
+                return false;
+
+            var reverted = 0;
+            while (n > reverted)
+            {
+                // Revert the number.
+                reverted = reverted * 10 + n % 10;
+
+                // Decrease the order of the input number.
+                n /= 10;
+            }
+
+            // When the length is an odd number, we can get rid of the middle digit by reverted/10
+            // For example when the input is 12321, at the end of the while loop we get n = 12, reverted = 123,
+            // since the middle digit doesn't matter in palidrome (it will always equal to itself), we can simply get rid of it.
+            return (n == reverted) || (n == reverted / 10);
+        }
+
+        // My solution.
+        public static bool IsPalindrome2(int n)
+        {
+            // Special case.
+            if (n != 0 && n % 10 == 0)
+                return false;
+
+            var r = 0;
+            while (r < n)
+            {
+                r += (n % 10);
+
+                if (r != n)
+                    n /= 10;
+                if (r < n)
+                    r *= 10;
+            }
+
+            return (r == n);
+        }
+
+        public static bool IsPalindromeUsingString(int n)
+        {
+            var s = n.ToString();
+
+            for (var i = 0; i < s.Length / 2; ++i)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ WbsAlgorithmsTest/Strings/PalindromeNumberTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using WbsAlgorithms.Strings;
+
+namespace WbsAlgorithmsTest.Strings
+{
+    public class PalindromeNumberTest
+    {
+        [TestCase(0, true)]
+        [TestCase(1, true)]
+        [TestCase(-1, false)]
+        [TestCase(10, false)]
+        [TestCase(11, true)]
+        [TestCase(12, false)]
+        [TestCase(21, false)]
+        [TestCase(100, false)]
+        [TestCase(101, true)]
+        [TestCase(121, true)]
+        [TestCase(-121, false)]
+        [TestCase(1221, true)]
+        [TestCase(1001, true)]
+        [TestCase(10001, true)]
+        [TestCase(12031, false)]
+        [TestCase(21012, true)]
+        [TestCase(210012, true)]
+        [TestCase(12321, true)]
+        public void IsPalindromeTest(int number, bool expectedValue)
+        {
+            var isPalindrome1 = PalindromeNumber.IsPalindrome1(number);
+            var isPalindrome2 = PalindromeNumber.IsPalindrome2(number);
+            var isPalindromeUsingString = PalindromeNumber.IsPalindromeUsingString(number);
+
+            Assert.That(isPalindrome1, Is.EqualTo(expectedValue));
+            Assert.That(isPalindrome2, Is.EqualTo(expectedValue));
+            Assert.That(isPalindromeUsingString, Is.EqualTo(expectedValue));
+        }
+    }
+}
